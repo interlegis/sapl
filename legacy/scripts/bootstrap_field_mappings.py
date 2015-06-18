f7708eb0851bb2283440605e4237afbbc790e591
Marcio Mazza <mazza@interlegis.leg.br>
2015-06-18 19:30:53 -0300

    Remove field mappings and add annotations check

--- legacy/scripts/bootstrap_field_mappings.py
+++ legacy/scripts/bootstrap_field_mappings.py
@@ -1,44 +1,16 @@
-from inspect import getsourcelines
-
 from collections import defaultdict
 from difflib import SequenceMatcher
-from os.path import dirname
-
-import yaml
+from inspect import getsourcelines
 
 from migration_base import *
 
 
-def print_field_mappings():
-    # output to bootstrap field_mappings.yaml
-    for app in appconfs:
-        print '\n%s\n%s:' % ('#' * 80, app.name)
-        for model in app.models.values():
-            print '\n  %s:' % model.__name__
-            new_fields = [f.name for f in model._meta.fields]
-            legacy_fields = [f.name for f in legacy_app.get_model(
-                model.__name__)._meta.fields if f.name != 'ind_excluido']
-            for pair in zip(new_fields, legacy_fields):
-                print '    %s : %s' % pair
+def is_field_line(line):
+    return not line.strip().startswith('#') and ' = ' in line
 
-    def similar(a, b):
-        return SequenceMatcher(None, a, b).ratio()
 
-    mappings = yaml.load(open(dirname(__file__) + '/field_mappings.yaml', 'r'))
-
-    different_pairs = defaultdict(list)
-    for map in mappings.values():
-        for name, fields in map.items():
-            for a, b in fields.items():
-                if a != 'id' and similar(a, b) < .7:
-                    different_pairs[name].append((a, b))
-
-    if different_pairs:
-        print '\n\n######## Different Pairs #########'
-        for name, pairs in different_pairs.items():
-            print '#', name
-            for a, b in pairs:
-                print '#  ', a, b
+def get_field(line):
+    return line.split('=')[0].strip()
 
 
 def print_commented_source(model):
@@ -46,15 +18,35 @@ def print_commented_source(model):
     legacy_fields = [f.name for f in legacy_app.get_model(model.__name__)._meta.fields if f.name != 'ind_excluido']
     new_to_old = dict(zip(new_fields, legacy_fields))
     lines = getsourcelines(model)[0]
-
-    def is_field(line):
-        return not line.strip().startswith('#') and ' = ' in line
-
-    cols = max(map(len, [line for line in lines if is_field(line)]))
+    cols = max(map(len, [line for line in lines if is_field_line(line)]))
     print '\n'
     for line in lines:
-        if not is_field(line):
+        if not is_field_line(line):
             print line.rstrip()
         else:
-            field = line.split('=')[0].strip()
-            print '%s # %s' % (line.rstrip().ljust(cols), new_to_old[field])
+            print '%s # %s' % (line.rstrip().ljust(cols), new_to_old[get_field(line)])
+    return new_to_old
+
+
+def check_similarity():
+
+    def similar(a, b):
+        return SequenceMatcher(None, a, b).ratio()
+
+    different_pairs = defaultdict(list)
+    for app in appconfs:
+        for model in app.models.values():
+            lines = getsourcelines(model)[0]
+            for line in lines:
+                if is_field_line(line):
+                    new = get_field(line)
+                    old = line.split('#')[-1].strip()
+                    if similar(new, old) < .7:
+                        different_pairs['%s (%s)' % (model.__name__, app.name)].append((new, old))
+
+    if different_pairs:
+        print '\n\n######## Different Pairs #########'
+        for name, pairs in different_pairs.items():
+            print name
+            for a, b in pairs:
+                print ' ', a, b
