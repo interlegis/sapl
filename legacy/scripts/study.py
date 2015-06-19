dcb60203a360104bb398e3085fa542050efb66c3
Marcio Mazza <mazza@interlegis.leg.br>
2015-06-19 11:04:31 -0300

    Add to legacy study

--- legacy/scripts/study.py
+++ legacy/scripts/study.py
@@ -1,5 +1,10 @@
 from django.apps import apps
 
+from migration_base import legacy_app
+
+
 for model in apps.get_app_config('legacy').get_models():
     if 'ind_excluido' in [f.name for f in model._meta.fields]:
         print model, model.objects.values_list('ind_excluido', flat=True).distinct()
+
+legacy_models_without_ind_excluido = [m for m in legacy_app.models.values() if not any(f.name == 'ind_excluido' for f in m._meta.fields)]
