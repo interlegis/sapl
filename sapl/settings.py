a4de0324950a64eec73c77807377fc6d52d2f38c
Marcio Mazza <marciomazza@gmail.com>
2015-06-21 10:32:49 -0300

    Set lang and default timezone

--- sapl/settings.py
+++ sapl/settings.py
@@ -117,15 +117,10 @@ DATABASE_ROUTERS = ['legacy.router.LegacyRouter', ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
