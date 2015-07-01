938bd5b10828019a41aae497bd5720a5adf2deba
Marcio Mazza <mazza@interlegis.leg.br>
2015-07-01 12:01:16 -0300

    List all fields by default in admin

--- base/admin.py
+++ base/admin.py
@@ -1,9 +1,11 @@
 from django.apps import apps
 from django.contrib import admin
 
-
 appname = __name__.split('.')[0]
 app = apps.get_app_config(appname)
-
 for model in app.get_models():
-    admin.site.register(model)
+    class CustomModelAdmin(admin.ModelAdmin):
+        list_display = [f.name for f in model._meta.fields if f.name != 'id']
+
+    if not admin.site.is_registered(model):
+        admin.site.register(model, CustomModelAdmin)
