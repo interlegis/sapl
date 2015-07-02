4f88012cab014083a1f85e1ea294332145e2f447
Marcio Mazza <mazza@interlegis.leg.br>
2015-07-02 17:48:38 -0300

    Adjust lexml model fields

--- lexml/models.py
+++ lexml/models.py
@@ -3,15 +3,15 @@ from django.db import models
 from django.utils.translation import ugettext as _
 
 
-class LexmlRegistroProvedor(models.Model):
+class LexmlProvedor(models.Model):  # LexmlRegistroProvedor
     id_provedor = models.IntegerField(verbose_name=_(u'Id do provedor'))                                                # id_provedor
-    nome_provedor = models.CharField(max_length=255, verbose_name=_(u'Nome do provedor'))                               # nom_provedor
-    sigla_provedor = models.CharField(max_length=15)                                                                    # sgl_provedor
-    adm_email = models.CharField(max_length=50, blank=True, null=True, verbose_name=_(u'E-mail do responsável'))        # adm_email
+    nome = models.CharField(max_length=255, verbose_name=_(u'Nome do provedor'))                               # nom_provedor
+    sigla = models.CharField(max_length=15)                                                                    # sgl_provedor
+    email_responsavel = models.CharField(max_length=50, blank=True, null=True, verbose_name=_(u'E-mail do responsável'))        # adm_email
     nome_responsavel = models.CharField(max_length=255, blank=True, null=True, verbose_name=_(u'Nome do responsável'))  # nom_responsavel
     tipo = models.CharField(max_length=50)                                                                              # tipo
     id_responsavel = models.IntegerField(blank=True, null=True, verbose_name=_(u'Id do responsável'))                   # id_responsavel
-    xml_provedor = models.TextField(blank=True, null=True, verbose_name=_(u'XML fornecido pela equipe do LexML:'))      # xml_provedor
+    xml = models.TextField(blank=True, null=True, verbose_name=_(u'XML fornecido pela equipe do LexML:'))      # xml_provedor
 
     class Meta:
         verbose_name = _(u'Provedor Lexml')
@@ -20,8 +20,8 @@ class LexmlRegistroProvedor(models.Model):
 
 class LexmlRegistroPublicador(models.Model):
     id_publicador = models.IntegerField(verbose_name=_(u'Id do publicador'))                                            # id_publicador
-    nome_publicador = models.CharField(max_length=255, verbose_name=_(u'Nome do publicador'))                           # nom_publicador
-    adm_email = models.CharField(max_length=50, blank=True, null=True, verbose_name=_(u'E-mail do responsável'))        # adm_email
+    nome = models.CharField(max_length=255, verbose_name=_(u'Nome do publicador'))                           # nom_publicador
+    email_responsavel = models.CharField(max_length=50, blank=True, null=True, verbose_name=_(u'E-mail do responsável'))        # adm_email
     sigla = models.CharField(max_length=255, blank=True, null=True, verbose_name=_(u'Sigla do Publicador'))                                                     # sigla
     nome_responsavel = models.CharField(max_length=255, blank=True, null=True, verbose_name=_(u'Nome do responsável'))  # nom_responsavel
     tipo = models.CharField(max_length=50)                                                                              # tipo
