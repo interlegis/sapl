489ab3244bdf3ea46a90c4233547e11b9ef45a07
Marcio Mazza <mazza@interlegis.leg.br>
2015-07-15 18:52:55 -0300

    Add base templates

--- sapl/settings.py
+++ sapl/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'sapl.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- sapl/urls.py
+++ sapl/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^comissoes/', include('comissoes.urls')),
 ]
