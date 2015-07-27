9aa5ac73a01f1b1da727b664095c37d56d58ed2a
Marcio Mazza <mazza@interlegis.leg.br>
2015-07-27 16:14:53 -0300

    Reformat code

--- comissoes/urls.py
+++ comissoes/urls.py
@@ -5,10 +5,11 @@ from comissoes.views import (
 
 
 urlpatterns = comissao_crud.urlpatterns + [
-    url(r'^(?P<pk>\d+)/composicao$', ComposicaoListView.as_view(), name='composicao'),
-    url(r'^(?P<pk>\d+)/materias$', MateriasListView.as_view(), name='materias'),
-    url(r'^(?P<pk>\d+)/reunioes$', ReunioesListView.as_view(), name='reunioes'),
-    # url(r'^materias$', MateriasListView.as_view(), name='materias'),
-    # url(r'^reunioes$', ReunioesListView.as_view(), name='reunioes'),
+    url(r'^(?P<pk>\d+)/composicao$',
+        ComposicaoListView.as_view(), name='composicao'),
+    url(r'^(?P<pk>\d+)/materias$',
+        MateriasListView.as_view(), name='materias'),
+    url(r'^(?P<pk>\d+)/reunioes$',
+        ReunioesListView.as_view(), name='reunioes'),
 ]
 comissoes_urls = urlpatterns, comissao_crud.namespace, comissao_crud.namespace
