e916cac054ff917b6cd2eb7c1a0dba2375f08909
Marcio Mazza <mazza@interlegis.leg.br>
2015-07-27 20:05:47 -0300

    Adjust static files settings

--- sapl/settings.py
+++ sapl/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from unipath import Path
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).ancestor(2)
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,26 +117,25 @@ LOCALE_PATHS = (
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.child("collected_static")
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    BASE_DIR.child("static"),
 )
-
-DAB_FIELD_RENDERER = \
-    'django_admin_bootstrapped.renderers.BootstrapFieldRenderer'
-
-CRISPY_TEMPLATE_PACK = 'foundation-5'
-CRISPY_FAIL_SILENTLY = not DEBUG
-
-BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, "bower")
-
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'djangobower.finders.BowerFinder',
 )
 
+MEDIA_ROOT = BASE_DIR.child("media")
+
+DAB_FIELD_RENDERER = \
+    'django_admin_bootstrapped.renderers.BootstrapFieldRenderer'
+CRISPY_TEMPLATE_PACK = 'foundation-5'
+CRISPY_FAIL_SILENTLY = not DEBUG
+
+BOWER_COMPONENTS_ROOT = BASE_DIR.child("bower")
 BOWER_INSTALLED_APPS = (
     'foundation',
     'foundation-datepicker',
