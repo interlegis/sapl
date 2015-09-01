d2665048e21db6719d9a821cc837aa8e35e801d7
LeandroRoberto <leandroroberto.br@gmail.com>
2015-09-01 10:57:42 -0300

    Class TipoNota inclusion

--- compilacao/models.py
+++ compilacao/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
-# Create your models here.
+class TipoNota(models.Model):
+    sigla = models.CharField(max_length=10, verbose_name=_('Sigla'))
+    nome = models.CharField(max_length=50, verbose_name=_('Nome'))
+    modelo = models.TextField(
+        blank=True, null=True, verbose_name=_('Modelo'))
+
+    class Meta:
+        verbose_name = _('Tipo de Nota')
+        verbose_name_plural = _('Tipos de Nota')
+
+    def __str__(self):
+        return self.sigla
