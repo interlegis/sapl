649e1aac74c2071d03ec9ab79d243ca919b5e79c
LeandroRoberto <leandroroberto.br@gmail.com>
2015-09-11 17:47:59 -0300

    last makemigrations

---
+++ compilacao/migrations/0003_auto_20150911_1735.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('compilacao', '0002_auto_20150907_2334'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='dispositivo',
+            name='dispositivo_pai',
+            field=models.ForeignKey(blank=True, null=True, verbose_name='Dispositivo Pai', to='compilacao.Dispositivo', related_name='filhos', default=None),
+        ),
+    ]
