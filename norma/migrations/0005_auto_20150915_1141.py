73a9f976b3fc9e37ea0283f3198443f2ac48cefa
LeandroRoberto <leandroroberto.br@gmail.com>
2015-09-15 14:21:13 -0300

    Basic error correction typing

---
+++ norma/migrations/0005_auto_20150915_1141.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('norma', '0004_auto_20150907_0004'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='normajuridica',
+            options={'verbose_name_plural': 'Normas Jurídicas', 'verbose_name': 'Norma Jurídica', 'ordering': ['-data']},
+        ),
+    ]
