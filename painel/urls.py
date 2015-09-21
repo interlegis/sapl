656ef1a58708195f7c67a3bf2a3d80d4d8d478d6
Eduardo Edson Batista Cordeiro Alves <eduardoalves@interlegis.leg.br>
2015-09-21 15:00:38 -0300

    Change links in Painel

--- painel/urls.py
+++ painel/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import include, url
 
 from .views import (controlador_painel, cronometro_painel_crud, json_presenca,
-                    json_votacao, painel_parlamentares_view, painel_view,
+                    json_votacao, paienl_mensagem_view,
+                    painel_parlamentares_view, painel_view,
                     painel_votacao_view)
 
 urlpatterns = [
     url(r'^sistema/painel$', painel_view),
     url(r'^sistema/painel/controlador',
         controlador_painel, name='controlador_painel'),
+    url(r'^sistema/painel/mensagem', paienl_mensagem_view),
     url(r'^sistema/painel/parlamentares', painel_parlamentares_view),
     url(r'^sistema/painel/votacao', painel_votacao_view),
     url(r'^sistema/painel/json_presenca', json_presenca, name='json_presenca'),
