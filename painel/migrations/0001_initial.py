718293cea4044bb5b25cffc45b094d500cf62a6c
Edward Ribeiro <eribeiro@interlegis.leg.br>
2015-09-30 18:05:14 -0300

    Rework Painel migrations

--- painel/migrations/0001_initial.py
+++ painel/migrations/0001_initial.py
@@ -13,20 +13,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Cronometro',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
-                ('start', models.PositiveIntegerField(verbose_name='Iniciar cronômetro')),
-                ('reset', models.PositiveIntegerField(verbose_name='Reiniciar cronômetro')),
-                ('stop', models.PositiveIntegerField(verbose_name='Parar cronômetro')),
-                ('data_painel', models.DateField(verbose_name='Data cronômetro')),
-                ('tipo', models.CharField(max_length=1, choices=[('A', 'Aparte'), ('D', 'Discurso'), ('O', 'Ordem do dia')], verbose_name='Tipo Cronômetro')),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('status', models.CharField(max_length=1, verbose_name='Status do cronômetro', choices=[('I', 'Start'), ('R', 'Reset'), ('S', 'Stop')], default='S')),
+                ('data_cronometro', models.DateField(verbose_name='Data do cronômetro')),
+                ('tipo', models.CharField(max_length=1, verbose_name='Tipo Cronômetro', choices=[('A', 'Aparte'), ('D', 'Discurso'), ('O', 'Ordem do dia')])),
             ],
         ),
         migrations.CreateModel(
             name='Painel',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
-                ('abrir', models.PositiveIntegerField(verbose_name='Abrir painel', default=0)),
-                ('fechar', models.PositiveIntegerField(verbose_name='Fechar painel', default=1)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('aberto', models.BooleanField(default=False, verbose_name='Abrir painel')),
                 ('data_painel', models.DateField(verbose_name='Data painel')),
                 ('mostrar', models.CharField(max_length=1, choices=[('C', 'Completo'), ('P', 'Parlamentares'), ('V', 'Votação'), ('M', 'Mensagem')], default='C')),
             ],
