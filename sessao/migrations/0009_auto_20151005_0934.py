28fb54b656db88cae1ef3761794763fc8875fd00
Edward Ribeiro <eribeiro@interlegis.leg.br>
2015-10-05 08:16:22 -0300

    Add boolean votacao_aberta to AbstractOrdemDia

---
+++ sessao/migrations/0009_auto_20151005_0934.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('sessao', '0008_auto_20151005_0814'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='expedientemateria',
+            old_name='votacao_iniciada',
+            new_name='votacao_aberta',
+        ),
+        migrations.RenameField(
+            model_name='ordemdia',
+            old_name='votacao_iniciada',
+            new_name='votacao_aberta',
+        ),
+    ]
