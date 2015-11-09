d9f4677ac380c6a0536ffb173c5444912c3d1a7d
Eduardo Edson Batista Cordeiro Alves <eduardoalves@interlegis.leg.br>
2015-11-09 16:36:45 -0200

    qa_check alerts fix

--- materia/urls.py
+++ materia/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+
 from materia.views import (DespachoInicialEditView, DespachoInicialView,
                            DocumentoAcessorioEditView, DocumentoAcessorioView,
                            FormularioCadastroView, FormularioSimplificadoView,
