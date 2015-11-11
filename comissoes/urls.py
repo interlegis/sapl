e25375b38ad7ef4a11c56ee174d3725fbd6ad5d6
Edward Ribeiro <eribeiro@interlegis.leg.br>
2015-11-11 14:02:12 -0200

    Add initial scaffolding of Comissao: inserir parlamentar

--- comissoes/urls.py
+++ comissoes/urls.py
@@ -2,11 +2,14 @@ from django.conf.urls import include, url
 
 from comissoes.views import (ComposicaoView, MateriasView, ReunioesView,
                              cargo_crud, comissao_crud,
+                             ComissaoParlamentarIncluirView,
                              periodo_composicao_crud, tipo_comissao_crud)
 
 comissao_url_patterns = comissao_crud.urlpatterns + [
     url(r'^(?P<pk>\d+)/composicao$',
         ComposicaoView.as_view(), name='composicao'),
+    url(r'^(?P<pk>\d+)/composicao/parlamentar',
+        ComissaoParlamentarIncluirView.as_view(), name='comissao_parlamentar'),
     url(r'^(?P<pk>\d+)/materias$',
         MateriasView.as_view(), name='materias'),
     url(r'^(?P<pk>\d+)/reunioes$',
