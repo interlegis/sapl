2fb27ac831c7440a41f991d91ac9f82e8189f01f
Marcio Mazza <mazza@interlegis.leg.br>
2016-02-03 09:59:24 -0200

    Remove print statements used for debugging

--- base/views.py
+++ base/views.py
@@ -26,7 +26,6 @@ class HelpView(TemplateView):
     # XXX treat non existing template as a 404!!!!
 
     def get_template_names(self):
-        print(self.kwargs['topic'])
         return ['ajuda/%s.html' % self.kwargs['topic']]
 
 
