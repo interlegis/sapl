5127590e9334f84a2fd37b1faafcee7c06204656
Marcio Mazza <mazza@interlegis.leg.br>
2016-02-16 18:39:45 -0200

    Adiciona script para ajustar campos char vazios

---
+++ scripts/convert_null_to_empty_in_all_char_fields.py
@@ -0,0 +1,17 @@
+from sapl.utils import appconfs
+from django.db import models
+
+
+def convert_null_to_empty():
+    for app in appconfs:
+        for model in app.get_models():
+            print('Convertendo null p/ vazio. model [%s]'
+                  % model._meta.model_name)
+            char_fields = [f for f in model._meta.fields
+                           if isinstance(f, (models.CharField,
+                                             models.TextField))]
+            for obj in model.objects.all():
+                for field in char_fields:
+                    if getattr(obj, field.name) is None:
+                        setattr(obj, field.name, '')
+                obj.save()
