085af96987b69bd3c462a568541e0a810bbb5026
Marcio Mazza <mazza@interlegis.leg.br>
2016-04-04 13:09:42 -0300

    Deixa o teste de urls de materia mais sucinto

--- materia/test_materia_urls.py
+++ materia/test_materia_urls.py
@@ -1,21 +1,22 @@
 from django.core.urlresolvers import reverse
+import pytest
 
 
-def test_urls_materia():
-    st = reverse('materia:pesquisar_materia_list')
-    assert st == '/materia/pesquisar-materia-list'
-
-    st = reverse('materia:relatoria_edit', kwargs={'pk': '11', 'id': '99'})
-    assert st == '/materia/11/relatoria/99/edit'
-
-    st = reverse('materia:tramitacao_edit', kwargs={'pk': '3', 'id': '8'})
-    assert st == '/materia/3/tramitacao/8/edit'
-
-    st = reverse('materia:adicionar_proposicao')
-    assert st == '/materia/proposicao'
-
-    st = reverse('materia:editar_proposicao', kwargs={'pk': '3'})
-    assert st == '/materia/proposicao/3/edit'
-
-    st = reverse('materia:list_proposicao')
-    assert st == '/materia/proposicao_list'
+@pytest.mark.parametrize("test_input,kwargs,expected", [
+    ('materia:pesquisar_materia_list',
+        {},
+        '/materia/pesquisar-materia-list'),
+    ('materia:relatoria_edit',
+        {'pk': '11', 'id': '99'},
+        '/materia/11/relatoria/99/edit'),
+    ('materia:tramitacao_edit',
+        {'pk': '3', 'id': '8'},
+        '/materia/3/tramitacao/8/edit'),
+    ('materia:adicionar_proposicao', {}, '/materia/proposicao'),
+    ('materia:editar_proposicao',
+        {'pk': '3'},
+        '/materia/proposicao/3/edit'),
+    ('materia:list_proposicao', {}, '/materia/proposicao_list'),
+])
+def test_reverse(test_input, kwargs, expected):
+    assert reverse(test_input, kwargs=kwargs) == expected
