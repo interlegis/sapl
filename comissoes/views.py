a6b6a6964610518d5262239995ecc0cc91bd5d4c
Eduardo Edson Batista Cordeiro Alves <eduardoalves@interlegis.leg.br>
2016-05-17 17:22:42 -0300

    Muda listagem de composicao

--- comissoes/views.py
+++ comissoes/views.py
@@ -24,7 +24,38 @@ class ComposicaoCrud(MasterDetailCrud):
     parent_field = 'comissao'
     help_path = ''
 
-    # class ListView(MasterDetailCrud.ListView):
+    class ListView(MasterDetailCrud.ListView):
+        template_name = 'comissoes/composicao_list.html'
+
+        def get_queryset(self):
+            return Participacao.objects.all().order_by('parlamentar')
+
+        def get(self, request, *args, **kwargs):
+            self.object_list = self.get_queryset()
+            form = ComposicaoForm(request.GET or None)
+
+            composicoes = Composicao.objects.filter(
+                comissao_id=self.kwargs['pk']).order_by('-periodo')
+            participacoes = Participacao.objects.all().order_by('parlamentar')
+
+            if composicoes:
+                composicao_id = composicoes.first().id
+                msg = ''
+            else:
+                composicao_id = 0
+                msg = _('Ainda não há uma composição formada!')
+                messages.add_message(request, messages.INFO, msg)
+
+            if request.GET:
+                composicao_id = int(form.data['periodo'])
+
+            return self.render_to_response({
+                'participacoes': participacoes,
+                'composicoes': composicoes,
+                'composicao_id': composicao_id,
+                'form': form,
+                'pk': self.kwargs['pk'],
+                'object': Comissao.objects.get(id=self.kwargs['pk'])})
 
 
 class ComissaoCrud(Crud):
