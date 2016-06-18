f18089e227f15adc2effc64aae572eae66d19c1a
LeandroRoberto <leandroroberto.br@gmail.com>
2016-06-17 23:33:59 -0300

    Move url media para sapl.urls

--- sapl/base/urls.py
+++ sapl/base/urls.py
@@ -23,7 +23,3 @@ urlpatterns = [
         name='login'),
     url(r'^logout/$', views.logout, {'next_page': '/login'}, name='logout')
 ]
-
-# Fix a static asset finding error on Django 1.9 + gunicorn:
-# http://stackoverflow.com/questions/35510373/
-urlpatterns += staticfiles_urlpatterns()
