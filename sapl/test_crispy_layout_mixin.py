064c1c4c9fe146ba114b8ce370aae8390474a7f0
Marcio Mazza <marciomazza@gmail.com>
2016-07-10 16:38:56 -0300

    Conserta teste do crispy layout mixin

--- sapl/test_crispy_layout_mixin.py
+++ sapl/test_crispy_layout_mixin.py
@@ -1,9 +1,13 @@
+from unittest import mock
+
+import rtyaml
+
 from sapl.crispy_layout_mixin import read_layout_from_yaml
 
 
 def test_read_layout_from_yaml(tmpdir):
 
-    contents = '''
+    stub_content = '''
 ModelName:
   Cool Legend:
   - name:9  place  tiny
@@ -12,18 +16,17 @@ ModelName:
   More data:
   - equalA  equalB  equalC
   - highlander '''
-    file = tmpdir.join('zzz.yaml')
-    file.write(contents)
 
-    expected = [
-        ['Cool Legend',
-         [('name', 9),  ('place', 2), ('tiny', 1)],
-         [('field', 10), ('nature', 2)],
-         [('kind', 1), ('date', 3), ('unit', 5), ('status', 3)],
-         ],
-        ['More data',
-         [('equalA', 4), ('equalB', 4), ('equalC', 4)],
-         [('highlander', 12)],
-         ],
-    ]
-    assert read_layout_from_yaml(file.strpath, 'ModelName') == expected
+    with mock.patch('sapl.crispy_layout_mixin.read_yaml_from_file') as ryff:
+        ryff.return_value = rtyaml.load(stub_content)
+        assert read_layout_from_yaml('....', 'ModelName') == [
+            ['Cool Legend',
+             [('name', 9),  ('place', 2), ('tiny', 1)],
+             [('field', 10), ('nature', 2)],
+             [('kind', 1), ('date', 3), ('unit', 5), ('status', 3)],
+             ],
+            ['More data',
+             [('equalA', 4), ('equalB', 4), ('equalC', 4)],
+             [('highlander', 12)],
+             ],
+        ]
