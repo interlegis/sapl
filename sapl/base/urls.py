8c79d99b57f619974305518d406e37c0088d0c89
Eduardo Edson Batista Cordeiro Alves <eduardoalves@interlegis.leg.br>
2016-08-25 15:04:40 -0300

    Adiciona template base para relatorios

--- sapl/base/urls.py
+++ sapl/base/urls.py
@@ -12,6 +12,8 @@ app_name = AppConfig.name
 urlpatterns = [
     url(r'^sistema/', TemplateView.as_view(template_name='sistema.html')),
     url(r'^ajuda/', TemplateView.as_view(template_name='ajuda.html')),
+    url(r'^relatorios/', TemplateView.as_view(
+        template_name='base/relatorios_list.html')),
     url(r'^ajuda/(?P<topic>\w+)$', HelpView.as_view(), name='help_topic'),
     url(r'^ajuda/', TemplateView.as_view(template_name='ajuda/index.html'),
         name='help_base'),
