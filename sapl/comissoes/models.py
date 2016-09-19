0d4f50ba53f704794b0aa15746046aa4336f1689
LeandroRoberto <leandroroberto.br@gmail.com>
2016-09-19 12:31:10 -0300

    Retira dupl do attr default, camp ativa em Comissao

--- sapl/comissoes/models.py
+++ sapl/comissoes/models.py
@@ -76,7 +76,6 @@ class Comissao(models.Model):
     ativa = models.BooleanField(
         default=False,
         choices=YES_NO_CHOICES,
-        default=True,
         verbose_name=_('Comissão Ativa?'))
 
     class Meta:
