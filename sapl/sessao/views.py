afea867160d8e974b6287a1d82d6f5064a89aaad
Eduardo Calil <dudusaid@gmail.com>
2016-09-22 14:49:07 -0300

    Fix #500

--- sapl/sessao/views.py
+++ sapl/sessao/views.py
@@ -2012,7 +2012,7 @@ class PesquisarSessaoPlenariaView(FilterView):
         qs = self.get_queryset()
 
         qs = qs.distinct().order_by(
-            '-legislatura__id', '-data_inicio', '-numero')
+            '-legislatura__numero', '-data_inicio', '-numero')
 
         kwargs.update({
             'queryset': qs,
