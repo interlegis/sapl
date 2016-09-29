19d44d674635b03ce9c0c0a6801afbe6ce68ebbe
Luciano Almeida <lucianoalmeida@interlegis.leg.br>
2016-09-29 12:01:19 -0300

    Adiciona comando migracao_25_31 ao manage.py
    Signed-off-by: Luciano Almeida <lucianoalmeida@interlegis.leg.br>

---
+++ sapl/legacy/management/commands/migracao_25_31.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+from sapl.legacy import migration
+
+
+class Command(BaseCommand):
+    help = u'Faz a migração de dados do SAPL 2.5 para o SAPL 3.1'
+
+    def handle(self, *args, **options):
+        migration.migrate()
