c068aa10ca0815ad9ed71e048169597390348d90
Eduardo Edson Batista Cordeiro Alves <eduardoalves@interlegis.leg.br>
2016-10-04 09:28:47 -0300

    Fix #682

--- sapl/sessao/models.py
+++ sapl/sessao/models.py
@@ -213,8 +213,8 @@ class AbstractOrador(models.Model):  # Oradores
     parlamentar = models.ForeignKey(Parlamentar, verbose_name=_('Parlamentar'))
     numero_ordem = models.PositiveIntegerField(
         verbose_name=_('Ordem de pronunciamento'))
-    url_discurso = models.URLField(
-        max_length=150, blank=True, verbose_name=_('URL Vídeo'))
+    url_discurso = models.CharField(
+        max_length=150, blank=True, verbose_name=_('URL Vídeo ou Observação'))
 
     class Meta:
         abstract = True
