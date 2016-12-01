95b138ac9f17f255b5b9231e9be53ae2427704e1
Edward Ribeiro <eribeiro@interlegis.leg.br>
2016-12-01 17:21:08 -0200

    Ajusta simple_gunicorn.sh para novo padrão

--- simple_gunicorn.sh
+++ simple_gunicorn.sh
@@ -1,9 +1,9 @@
-DJANGODIR=/home/sapl31/sapl                     # Django project directory (*)
+DJANGODIR=/var/interlegis/sapl                     # Django project directory (*)
 DJANGO_SETTINGS_MODULE=sapl.settings            # which settings file should Django use (*)
 DJANGO_WSGI_MODULE=sapl.wsgi                    # WSGI module name (*)
 
 cd $DJANGODIR
-source ~/.virtualenvs/sapl/bin/activate
+source /var/interlegis/.virtualenvs/sapl/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
