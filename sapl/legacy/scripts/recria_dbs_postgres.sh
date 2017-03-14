dd1351d70f752ab0231511913e80828400e061ed
Marcio Mazza <marciomazza@gmail.com>
2017-03-14 14:17:58 -0300

    Adapta migração para rodar em paralelo

---
+++ sapl/legacy/scripts/recria_dbs_postgres.sh
@@ -0,0 +1,5 @@
+# (Re)cria todos os bancos postgres para migração
+# cria um banco postgres (de mesmo nome) para cada banco mysql cujo nome começa com "sapl_"
+
+mysql -u root -padmin -e 'show databases;' | grep '^sapl_' | xargs -I{} ./recria_um_db_postgres.sh {}
+
