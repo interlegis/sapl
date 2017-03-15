9e625008bd640b10fa523ebf4f5311133d0adc9e
Eduardo Calil <dudusaid@gmail.com>
2017-03-15 12:56:48 -0300

    Coloca FK para ProtocoloAdm em DocAdm e faz script de substituicao

---
+++ scripts/fk_protocoloadm_docadm.py
@@ -0,0 +1,22 @@
+# Esse script foi feito para substituir a referência a Protocolo
+# em algum Documento, que antes era numero e ano, para uma FK
+
+
+from django.core.exceptions import ObjectDoesNotExist
+from sapl.protocoloadm.models import DocumentoAdministrativo, Protocolo
+
+
+def substitui():
+    for d in DocumentoAdministrativo.objects.all():
+        if d.numero_protocolo:
+            try:
+                d.protocolo = Protocolo.objects.get(
+                    ano=d.ano,
+                    numero=d.numero_protocolo)
+                d.save()
+            except ObjectDoesNotExist:
+                return
+
+
+if __name__ == '__main__':
+    substitui()
