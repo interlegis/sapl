12a3d6995e25201e5035ec9e95659883f66422b0
Edward Ribeiro <eribeiro@interlegis.leg.br>
2017-04-10 14:18:22 -0300

    Modifica scripts para voltar diretório padrão do gunicorn

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -2,9 +2,18 @@
 
 # As seen in http://tutos.readthedocs.org/en/latest/source/ndg.html
 
+SAPL_DIR=/var/interlegis/sapl
+
+# Seta um novo diretório foi passado como raiz para o SAPL
+# caso esse tenha sido passado como parâmetro
+if [ "$1" ]
+then
+    SAPL_DIR="$1"
+fi
+
 NAME="SAPL"                                     # Name of the application (*)
-DJANGODIR=/sapl/                    # Django project directory (*)
-SOCKFILE=/sapl/run/gunicorn.sock    # we will communicate using this unix socket (*)
+DJANGODIR=$SAPL_DIR                    # Django project directory (*)
+SOCKFILE=$SAPL_DIR/gunicorn.sock    # we will communicate using this unix socket (*)
 USER=`whoami`                                   # the user to run as (*)
 GROUP=`whoami`                                  # the group to run as (*)
 NUM_WORKERS=3                                   # how many worker processes should Gunicorn spawn (*)
