a27ebc13c5b0388c701578961de5a1385f72b299
Edward Ribeiro <eribeiro@interlegis.leg.br>
2017-04-19 10:46:05 -0300

    Substitui gen-env.py por shell script puro

--- start.sh
+++ start.sh
@@ -1,6 +1,44 @@
 #!/bin/sh
 
-python3 gen-env.py
+
+create_env() {
+    # check if file exists
+    if [ ! -f "data/secret.key" ]; then   
+        KEY=`cat data/secret.key`
+    else
+        KEY=`python3 genkey.py`
+        echo $KEY > data/secret.key
+    fi
+
+    # TODO: rename env-test-bash to .env
+    FILENAME="sapl/.env"
+
+    if [ -z "${DATABASE_URL:-}" ]; then
+        DATABASE_URL="postgresql://sapl:sapl@sapldb:5432/sapl"
+    fi
+
+    # ALWAYS replace the content of .env variable
+    # If want to conditionally create only if absent then use IF below
+    #    if [ ! -f $FILENAME ]; then
+
+    touch $FILENAME
+
+    # explicitly use '>' to erase any previous content
+    echo "SECRET_KEY="$KEY > $FILENAME
+    # now only appends
+    echo "DATABASE_URL = "$DATABASE_URL >> $FILENAME
+    echo "DEBUG = False" >> $FILENAME
+    echo "EMAIL_USE_TLS = True" >> $FILENAME
+    echo "EMAIL_PORT = 587" >> $FILENAME
+    echo "EMAIL_HOST = ""${EMAIL_HOST-''}" >> $FILENAME
+    echo "EMAIL_HOST_USER = ""${EMAIL_HOST_USER-''}" >> $FILENAME
+    echo "EMAIL_HOST_PASSWORD = ""${EMAIL_HOST_PASSWORD-''}" >> $FILENAME
+}
+
+create_env
+
+# python3 gen-env.py
+
 python3 manage.py bower install
 
 /bin/sh busy-wait.sh
