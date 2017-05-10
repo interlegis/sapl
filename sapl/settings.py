12c854c6e412d5e4ccd0ee9ba02deb76798c15e8
Eduardo Calil <dudusaid@gmail.com>
2017-05-10 15:11:39 -0300

    Faz mudanças sugeridas

--- sapl/settings.py
+++ sapl/settings.py
@@ -88,7 +88,7 @@ INSTALLED_APPS = (
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-        'URL': 'http://127.0.0.1:8983/solr'
+        'URL': config('SOLR_URL', cast=str)
         # ...or for multicore...
         # 'URL': 'http://127.0.0.1:8983/solr/mysite',
     },
