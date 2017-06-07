e450f194aacedf751d8a6e1b2227e2e4848f06d6
Eduardo Calil <dudusaid@gmail.com>
2017-06-07 13:24:01 -0300

    Tentativa de passar no travis

--- sapl/sessao/models.py
+++ sapl/sessao/models.py
@@ -405,18 +405,11 @@ class RegistroVotacao(models.Model):
         verbose_name_plural = _('Votações')
 
     def __str__(self):
-        if self.ordem:
-            return _('Ordem: %(ordem)s - Votação: %(votacao)s - '
-                     'Matéria: %(materia)s') % {
-                         'ordem': self.ordem,
-                         'votacao': self.tipo_resultado_votacao,
-                'materia': self.materia}
-        elif self.expediente:
-            return _('Expediente: %(expediente)s - Votação: %(votacao)s - '
-                     'Matéria: %(materia)s') % {
-                         'expediente': self.expediente,
-                         'votacao': self.tipo_resultado_votacao,
-                'materia': self.materia}
+        return _('Ordem: %(ordem)s - Votação: %(votacao)s - '
+                 'Matéria: %(materia)s') % {
+                     'ordem': self.ordem,
+                     'votacao': self.tipo_resultado_votacao,
+            'materia': self.materia}
 
 
 @reversion.register()
