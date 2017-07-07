e5b44bdec21bb48d9aef92e7f0f925b71bf3abfa
Eduardo Calil <dudusaid@gmail.com>
2017-07-07 17:04:12 -0300

    Cria o campo de data de inicio do mandato

---
+++ scripts/set_inicio_mandato.py
@@ -0,0 +1,10 @@
+from sapl.parlamentares.models import Mandato
+
+
+def popula_campo_data_inicio():
+    for m in Mandato.objects.all():
+        m.data_inicio_mandato = m.legislatura.data_inicio
+        m.save()
+
+if __name__ == '__main__':
+    popula_campo_data_inicio()
