914e2eff6d8b0ce2e2990dd6ec7a539beebba8b0
Edward Ribeiro <eribeiro@interlegis.leg.br>
2017-07-12 13:07:09 -0300

    HOT-FIX: remove imports não mais utilizados.

--- sapl/materia/signals.py
+++ sapl/materia/signals.py
@@ -2,8 +2,6 @@ from django.db.models.signals import post_delete, post_save
 
 import django.dispatch
 
-from sapl.utils import delete_texto, save_texto
-
 from .models import DocumentoAcessorio, MateriaLegislativa
 
 
--- sapl/norma/signals.py
+++ sapl/norma/signals.py
@@ -1,5 +1,3 @@
 from django.db.models.signals import post_delete, post_save
 
-from sapl.utils import delete_texto, save_texto
-
 from .models import NormaJuridica
