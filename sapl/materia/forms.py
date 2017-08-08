50df4c157b57c74d7fcfa328e8fb5fcdc41a519f
DTILegBG <cpd@camarabento.rs.gov.br>
2017-08-08 16:55:33 -0300

    ajuste teste do travis

--- sapl/materia/forms.py
+++ sapl/materia/forms.py
@@ -165,9 +165,6 @@ class AcompanhamentoMateriaForm(ModelForm):
 
 
 class DocumentoAcessorioForm(ModelForm):
-    data = forms.DateField(required=True)
-    arquivo = forms.FileField(required=True)
-
 
     class Meta:
         model = DocumentoAcessorio
