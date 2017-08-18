1ccec7c98f7e07522c421583be8748aaada67d9e
Mariana Mendes <mare.s.mendes@gmail.com>
2017-08-18 16:20:28 -0300

    Corrige erro do import de Filiacao

--- sapl/base/templatetags/common_tags.py
+++ sapl/base/templatetags/common_tags.py
@@ -4,7 +4,7 @@ from django import template
 from sapl.base.models import AppConfig
 from sapl.materia.models import DocumentoAcessorio, MateriaLegislativa
 from sapl.norma.models import NormaJuridica
-from sapl.parlamentares import Filiacao
+from sapl.parlamentares.models import Filiacao
 from sapl.utils import filiacao_data
 
 register = template.Library()
