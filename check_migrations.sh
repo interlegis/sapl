dfefe2f2db8e3b5b348793464371a6b4a16f7de9
Marcio Mazza <marciomazza@gmail.com>
2017-08-21 15:44:41 -0300

    Adiciona script pre-commit do git

--- check_migrations.sh
+++ check_migrations.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# TODO: Após migrar para Django 1.10 usar
+#
+#       ./manage.py makemigrations --check --dry-run
+#
+# ATENÇÃO: a chamada atual termina com exit 1 se NÃO HÁ migração a ser aplicada
+#          ou seja, termina com "erro" se está tudo bem!
+#          A chamada do django 1.10 INVERTE ISSO.
+#
+# https://docs.djangoproject.com/en/1.10/ref/django-admin/#cmdoption-makemigrations-check
+
 python manage.py makemigrations --dry-run --exit
 
 MIGRATIONS=$?
@@ -7,11 +17,10 @@ MIGRATIONS=$?
 NC='\033[0m'
 
 if [ $MIGRATIONS -eq 0 ]; then
-	RED='\033[0;31m'
-	echo -e "${RED}You have unapplied code changes. run 'python manage.py makemigrations' before submitting your code...${NC}"
-	exit 1
-else
-	GREEN='\033[0;32m'
-	echo -e "Working normally..."
-	exit 0
+    RED='\033[0;31m'
+    echo
+    echo -e "${RED}ALGUMAS ALTERAÇÕES EXIGEM MIGRAÇÃO.${NC}"
+    echo -e "${RED}RODE 'python manage.py makemigrations' ANTES DE SUBMETER SEU CÓDIGO...${NC}"
+    echo
+    exit 1
 fi
---
+++ scripts/hooks/instalar_hooks_git.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln -s -f `pwd`/scripts/hooks/pre-commit .git/hooks/pre-commit
+
+
