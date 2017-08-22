2890e862830b39c625b989b13652b30faa271800
Eduardo Calil <dudusaid@gmail.com>
2017-08-22 17:45:38 -0300

    Implementa Impressos de Etiquetas

--- sapl/materia/forms.py
+++ sapl/materia/forms.py
@@ -1595,3 +1595,87 @@ class MateriaAssuntoForm(ModelForm):
         fields = ['materia', 'assunto']
 
         widgets = {'materia': forms.HiddenInput()}
+
+
+class EtiquetaPesquisaForm(forms.Form):
+    tipo_materia = forms.ModelChoiceField(
+        label=TipoMateriaLegislativa._meta.verbose_name,
+        queryset=TipoMateriaLegislativa.objects.all(),
+        required=False,
+        empty_label='Selecione')
+
+    data_inicial = forms.DateField(
+        label='Data Inicial',
+        required=False,
+        widget=forms.DateInput(format='%d/%m/%Y')
+    )
+
+    data_final = forms.DateField(
+        label='Data Final',
+        required=False,
+        widget=forms.DateInput(format='%d/%m/%Y')
+    )
+
+    processo_inicial = forms.IntegerField(
+        label='Processo Inicial',
+        required=False)
+
+    processo_final = forms.IntegerField(
+        label='Processo Final',
+        required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(EtiquetaPesquisaForm, self).__init__(*args, **kwargs)
+
+        row1 = to_row(
+            [('tipo_materia', 6),
+             ('data_inicial', 3),
+             ('data_final', 3)])
+
+        row2 = to_row(
+            [('processo_inicial', 6),
+             ('processo_final', 6)])
+
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Fieldset(
+                ('Formulário de Etiqueta'),
+                row1, row2,
+                form_actions(save_label='Pesquisar')
+            )
+        )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        # Verifica se algum campo de data foi preenchido
+        if cleaned_data['data_inicial'] or cleaned_data['data_final']:
+            # Então verifica se o usuário preencheu o Incial e mas não
+            # preencheu o Final, ou vice-versa
+            if (not cleaned_data['data_inicial'] or
+               not cleaned_data['data_final']):
+                raise ValidationError(_(
+                    'Caso pesquise por data, os campos de Data Incial e ' +
+                    'Data Final devem ser preenchidos obrigatoriamente'))
+            # Caso tenha preenchido, verifica se a data final é maior que
+            # a inicial
+            elif cleaned_data['data_final'] < cleaned_data['data_inicial']:
+                raise ValidationError(_(
+                    'A Data Final não pode ser menor que a Data Inicial'))
+
+        # O mesmo processo anterior é feito com o processo
+        if (cleaned_data['processo_inicial'] or
+           cleaned_data['processo_final']):
+            if (not cleaned_data['processo_inicial'] or
+               not cleaned_data['processo_final']):
+                raise ValidationError(_(
+                    'Caso pesquise por número de processo, os campos de ' +
+                    'Processo Inicial e Processo Final ' +
+                    'devem ser preenchidos obrigatoriamente'))
+            elif (cleaned_data['processo_final'] <
+                  cleaned_data['processo_inicial']):
+                    raise ValidationError(_(
+                        'O processo final não pode ser menor que o inicial'))
+
+        return cleaned_data
+
