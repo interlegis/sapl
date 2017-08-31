1d8e0839684593443bfae7bbb55782cedb07112e
Mariana Mendes <mare.s.mendes@gmail.com>
2017-08-31 12:01:17 -0300

    Adiciona teste em NotaForm

--- sapl/compilacao/tests/test_tipo_texto_articulado_form.py
+++ sapl/compilacao/tests/test_tipo_texto_articulado_form.py
@@ -1,9 +1,10 @@
 import pytest
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from model_mommy import mommy
 
 from sapl.compilacao import forms
-from sapl.compilacao.models import PerfilEstruturalTextoArticulado
+from sapl.compilacao.models import (Nota, PerfilEstruturalTextoArticulado,
+                                    TipoNota)
 from sapl.compilacao.views import choice_models_in_extenal_views
 
 
@@ -39,3 +40,37 @@ def test_tipo_texto_articulado_form_valid(content_type):
                                   })
 
     assert form.is_valid(), form.errors
+
+def test_valida_campos_obrigatorios_nota_form():
+    form = forms.NotaForm(data={})
+
+    assert not form.is_valid()
+
+    errors = form.errors
+
+    assert errors['texto'] == [_('Este campo é obrigatório')]
+    assert errors['publicidade'] == [_('Este campo é obrigatório.')]
+    assert errors['tipo'] == [_('Este campo é obrigatório.')]
+    assert errors['publicacao'] == [_('Este campo é obrigatório')]
+    assert errors['efetividade'] == [_('Este campo é obrigatório')]
+    assert errors['dispositivo'] == [_('Este campo é obrigatório.')]
+
+    assert len(errors) == 6
+
+
+@pytest.mark.django_db(transaction=False)
+def test_nota_form_valid():
+    tipo = mommy.make(TipoNota)
+
+    form = forms.NotaForm(data={'titulo': 'titulo',
+                                'texto': 'teste',
+                                'url_externa': 'www.test.com',
+                                'publicidade': 'publicidade',
+                                'tipo': str(tipo.pk),
+                                'publicacao': '10/05/2017',
+                                'efetividade': '10/05/2017',
+                                'dispositivo': 'dispositivo',
+                                'pk': 'pk'
+                                })
+
+    assert form.is_valid()
