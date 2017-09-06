635552c825f46753d67db14396b774bc0602df9d
Eduardo Calil <dudusaid@gmail.com>
2017-09-06 19:23:29 -0300

    Fix #1460

--- sapl/parlamentares/forms.py
+++ sapl/parlamentares/forms.py
@@ -26,7 +26,7 @@ class ImageThumbnailFileInput(ClearableFileInput):
 def validar_datas_legislatura(eleicao, inicio, fim, pk=None):
 
     # Verifica se data de eleição < inicio < fim
-    if inicio >= fim or eleicao >= inicio:
+    if inicio > fim or eleicao > inicio:
         msg_error = _('A data início deve ser menor que a ' +
                       'data fim, e a data eleição deve ser ' +
                       'menor que a data início')
@@ -108,52 +108,16 @@ class LegislaturaForm(ModelForm):
         data_fim = data['data_fim']
         data_eleicao = data['data_eleicao']
 
-        if data_eleicao.year >= data_inicio.year:
-            raise ValidationError(_("Data eleição não pode ser inferior a "
-                                    "data início da legislatura"))
+        pk = self.instance.pk
 
-        if data_inicio > data_fim or (data_fim.year - data_inicio.year != 4):
-            raise ValidationError(_("Intervalo de início e fim inválido para "
-                                    "legislatura."))
+        valida_datas = validar_datas_legislatura(data_eleicao,
+                                                 data_inicio,
+                                                 data_fim,
+                                                 pk=pk)
+        if not valida_datas[0]:
+            raise ValidationError(valida_datas[1])
 
         return data
-
-
-class LegislaturaCreateForm(LegislaturaForm):
-
-    def clean(self):
-        super(LegislaturaCreateForm, self).clean()
-
-        cleaned_data = self.cleaned_data
-
-        if not self.is_valid():
-            return cleaned_data
-
-        eleicao = cleaned_data['data_eleicao']
-        inicio = cleaned_data['data_inicio']
-        fim = cleaned_data['data_fim']
-
-        valida_datas = validar_datas_legislatura(eleicao, inicio, fim)
-        if not valida_datas[0]:
-            raise ValidationError(valida_datas[1])
-        return cleaned_data
-
-
-class LegislaturaUpdateForm(LegislaturaCreateForm):
-
-    def clean(self):
-        super(LegislaturaUpdateForm, self).clean()
-
-        cleaned_data = super(LegislaturaCreateForm, self).clean()
-        eleicao = cleaned_data['data_eleicao']
-        inicio = cleaned_data['data_inicio']
-        fim = cleaned_data['data_fim']
-
-        valida_datas = validar_datas_legislatura(
-            eleicao, inicio, fim, pk=self.instance.pk)
-        if not valida_datas[0]:
-            raise ValidationError(valida_datas[1])
-        return cleaned_data
 
 
 class ParlamentarForm(ModelForm):
