96c61dd5cdce051252e87d93071f62be78475f0c
Marcio Mazza <marciomazza@gmail.com>
2017-09-11 17:16:05 -0300

    Configura fonte de destino da migracao em .env

--- sapl/legacy_migration_settings.py
+++ sapl/legacy_migration_settings.py
@@ -12,14 +12,18 @@ INSTALLED_APPS += (
     'sapl.legacy',  # legacy reversed model definitions
 )
 
-DATABASES['legacy'] = config('DATABASE_URL', cast=db_url,)
+DATABASES['legacy'] = config('DATABASE_URL_FONTE', cast=db_url,)
+DATABASES['default'] = config('DATABASE_URL_DESTINO', cast=db_url,
+                              default=DATABASES['default'])
 
 # Sobrescreve o nome dos bancos caso a variável de ambiente seja definida
 # Útil para migração em lote de vários bancos
 DATABASE_NAME_OVERRIDE = os.environ.get('DATABASE_NAME')
 if DATABASE_NAME_OVERRIDE:
-    for db in DATABASES.values():
-        db['NAME'] = DATABASE_NAME_OVERRIDE
+    DATABASES['legacy']['NAME'] = DATABASE_NAME_OVERRIDE
+    # não altera o nome se o destino é um banco em memória
+    if not DATABASES['default']['NAME'] == ':memory:':
+        DATABASES['default']['NAME'] = DATABASE_NAME_OVERRIDE
 
 DATABASE_ROUTERS = ['sapl.legacy.router.LegacyRouter', ]
 
