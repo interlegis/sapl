fd91e3c4bbb07deaf2a05d82716e1eae10aac765
Mariana Mendes <mare.s.mendes@gmail.com>
2017-09-18 17:13:57 -0300

    Adiciona teste de vie em SessaoPlenaria

--- sapl/sessao/tests/test_sessao_view.py
+++ sapl/sessao/tests/test_sessao_view.py
@@ -3,30 +3,30 @@ from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from model_mommy import mommy
 
-from sapl.parlamentares.models import Legislatura, SessaoLegislativa
+from sapl.parlamentares.models import Legislatura, Partido, SessaoLegislativa
+from sapl.sessao import forms
 from sapl.sessao.models import SessaoPlenaria, TipoSessaoPlenaria
 
-
-@pytest.mark.django_db(transaction=False)
+pytest.mark.django_db(transaction=False)
 def test_incluir_sessao_plenaria_submit(admin_client):
-    legislatura = mommy.make(Legislatura)
-    sessao = mommy.make(SessaoLegislativa)
-    tipo = mommy.make(TipoSessaoPlenaria)
+        legislatura = mommy.make(Legislatura)
+        sessao = mommy.make(SessaoLegislativa)
+        tipo = mommy.make(TipoSessaoPlenaria)
 
-    response = admin_client.post(reverse('sapl.sessao:sessaoplenaria_create'),
-                                 {'legislatura': str(legislatura.pk),
-                                  'numero': '1',
-                                  'tipo': str(tipo.pk),
-                                  'sessao_legislativa': str(sessao.pk),
-                                  'data_inicio': '10/11/2017',
-                                  'hora_inicio': '10:10'
-                                  }, follow=True)
+        response = admin_client.post(reverse('sapl.sessao:sessaoplenaria_create'),
+                                     {'legislatura': str(legislatura.pk),
+                                      'numero': '1',
+                                      'tipo': str(tipo.pk),
+                                      'sessao_legislativa': str(sessao.pk),
+                                      'data_inicio': '10/11/2017',
+                                      'hora_inicio': '10:10'
+                                      },
+                                      follow=True)
 
-    assert response.status_code == 200
+        assert response.status_code == 200
 
-    sessao_plenaria = SessaoPlenaria.objects.first()
-    assert sessao_plenaria.tipo == tipo
-
+        sessao = SessaoPlenaria.objects.first()
+        assert sessao.tipo == tipo
 
 @pytest.mark.django_db(transaction=False)
 def test_incluir_sessao_errors(admin_client):
