c0de3223686aff649be1ee6aadfc931835c3e53a
Eliseu Egewarth <eliseuegewarth@gmail.com>
2017-09-18 16:06:44 -0300

    Adiciona argumento senha aos scripts de migração
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- sapl/legacy/scripts/recria_dbs_postgres.sh
+++ sapl/legacy/scripts/recria_dbs_postgres.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
+
 # (Re)cria todos os bancos postgres para migração
 # cria um banco postgres (de mesmo nome) para cada banco mysql cujo nome começa com "sapl_"
 
-
 if [ $# -eq 2 ]; then
-    parallel --verbose ./recria_um_db_postgres.sh :::: <(mysql -u $1 -p$2 -e 'show databases;' | grep '^sapl_' | grep -v '_copy$')
+    parallel --verbose -j+0 ./recria_um_db_postgres.sh :::: <(mysql -u $1 -p$2 -e 'show databases;' | grep '^sapl_' | grep -v '_copy$')
 else
     echo "USO:"
-    echo "    ./recria_dbs_postgres.sh [usuário mysql] [senha mysql]"
+    echo "    $0 [usuário mysql] [senha mysql]"
 fi;
-
