4a8d00917cd867ddd45d97137fde90f40cdc0750
Edward Ribeiro <eribeiro@interlegis.leg.br>
2017-12-07 14:02:07 -0200

    Revert "cria impresso de indice de normas (#1625)"
    This reverts commit bf6e60d23bdf463e5034d03fa1bea7a0dce72795.

--- sapl/materia/urls.py
+++ sapl/materia/urls.py
@@ -25,8 +25,6 @@ from sapl.materia.views import (AcompanhamentoConfirmarView,
                                 TramitacaoEmLoteView, UnidadeTramitacaoCrud,
                                 proposicao_texto, recuperar_materia)
 
-from sapl.norma.views import  NormaPesquisaView
-
 from .apps import AppConfig
 
 app_name = AppConfig.name
@@ -44,9 +42,6 @@ urlpatterns_impressos = [
     url(r'^materia/impressos/ficha-seleciona/$',
         FichaSelecionaView.as_view(),
         name='impressos_ficha_seleciona'),
-    url(r'^materia/impressos/norma-pesquisa/$',
-        NormaPesquisaView.as_view(),
-        name='impressos_norma_pesquisa'),
 ]
 
 urlpatterns_materia = [
