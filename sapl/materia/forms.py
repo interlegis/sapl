f18f9537c01dcde360c37e70fa1069b5b4f340b3
Edward Ribeiro <eribeiro@interlegis.leg.br>
2017-12-13 14:49:42 -0200

    Revert "HOT-FIX: substituir 0 e 1 por False e True"
    This reverts commit 4294c9fc0778d908f6ec17dfa4b5b5157ec10b14.

--- sapl/materia/forms.py
+++ sapl/materia/forms.py
@@ -54,8 +54,8 @@ def ANO_CHOICES():
 
 def em_tramitacao():
     return [('', 'Tanto Faz'),
-            (True, 'Sim'),
-            (False, 'Não')]
+            (1, 'Sim'),
+            (0, 'Não')]
 
 
 class AdicionarVariasAutoriasFilterSet(django_filters.FilterSet):
