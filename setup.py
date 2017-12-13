5bb6099412ca7a29409ea1d6dae94ea79e58c60d
Edward Ribeiro <eribeiro@interlegis.leg.br>
2017-12-13 18:20:30 -0200

    Novo release: 3.1.41-BETA

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ install_requires = [
 ]
 setup(
     name='interlegis-sapl',
-    version='3.1.40-BETA',
+    version='3.1.41-BETA',
     packages=find_packages(),
     include_package_data=True,
     license='GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007',
