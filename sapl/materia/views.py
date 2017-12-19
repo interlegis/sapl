7b2e3914749a9ff819f5cac29bb5b7f65336ab2e
Edward Ribeiro <eribeiro@interlegis.leg.br>
2017-12-19 16:57:26 -0200

    HOT-FIX: diminui o tamanho para acelerar renderização do barcode

--- sapl/materia/views.py
+++ sapl/materia/views.py
@@ -827,7 +827,7 @@ class ReciboProposicaoView(TemplateView):
             _hash = 'P' + ta.hash() + '/' + str(proposicao.id)
 
         from sapl.utils import create_barcode
-        base64_data = create_barcode(_hash, 100, 2000)
+        base64_data = create_barcode(_hash, 100, 200)
         barcode = 'data:image/png;base64,{0}'.format(base64_data)
 
 
