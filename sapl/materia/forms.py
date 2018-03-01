e9cd10b502da9cf998080a17eb548a8e4d041619
Mariana Mendes <mare.s.mendes@gmail.com>
2018-03-01 09:32:26 -0300

    Muda o layout do create de reunião

--- sapl/materia/forms.py
+++ sapl/materia/forms.py
@@ -1265,6 +1265,7 @@ class ProposicaoForm(forms.ModelForm):
                         inst.texto_original.delete()
             self.gerar_hash(inst, receber_recibo)
 
+
             return super().save(commit)
 
         inst.ano = timezone.now().year
