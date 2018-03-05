81ec096c0c9469cc002836e338de1b3952d8d763
Mariana Mendes <mare.s.mendes@gmail.com>
2018-03-05 16:00:26 -0300

    Corrige o update, delete e list view.

--- sapl/audiencia/views.py
+++ sapl/audiencia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 from django.db.models import F
+from django.views.decorators.clickjacking import xframe_options_exempt
 from django.views.generic import ListView
 from sapl.comissoes.forms import ParticipacaoCreateForm, ParticipacaoEditForm
 from sapl.crud.base import RP_DETAIL, RP_LIST, Crud, CrudAux, MasterDetailCrud
@@ -18,14 +19,29 @@ class AudienciaCrud(Crud):
     public = [RP_LIST, RP_DETAIL, ]
 
     class BaseMixin(Crud.BaseMixin):
-        list_field_names = ['materia', 'tipo', 'numero', 'nome', 'tema',
-                            'data', 'hora_inicio', 'hora_fim', 'observacao',
-                            'url_audio', 'url_video', 'upload_pauta',
-                            'upload_ata']
+        list_field_names = ['materia', 'tipo', 'numero', 'nome',
+                            'data']
         ordering = 'nome', 'numero', 'tipo', 'data'
+
+    class ListView(Crud.ListView):
+        paginate_by = 10
 
     class CreateView(Crud.CreateView):
         form_class = AudienciaForm
 
         def form_valid(self, form):
             return super(Crud.CreateView, self).form_valid(form)
+
+    class UpdateView(Crud.UpdateView):
+        form_class = AudienciaForm
+     
+    class DeleteView(Crud.DeleteView):
+        pass
+
+    class DetailView(Crud.DetailView):
+
+        @xframe_options_exempt
+        def get(self, request, *args, **kwargs):
+            return super().get(request, *args, **kwargs)
+
+    
