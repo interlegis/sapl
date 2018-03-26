db51ab5f81dd87578e2e77a5bc351b9f30e79dc7
Marcio Mazza <marciomazza@gmail.com>
2018-03-26 13:46:43 -0300

    Retira código desnecessário
    Este código foi usado para gerar uma versão inicial dos forms
    com diagramanção semelhante à do sapl 2.5

--- sapl/legacy/scripts/utils.py
+++ sapl/legacy/scripts/utils.py
@@ -4,16 +4,11 @@ from sapl.base.models import Autor
 from sapl.legacy.migracao_dados import appconfs
 
 
-def getsourcelines(model):
-    return [line.rstrip('\n').decode('utf-8')
-            for line in inspect.getsourcelines(model)[0]]
-
-
 def get_models_com_referencia_a(apontado):
 
-    def tem_referencia_a_autor(model):
+    def tem_referencia_a_apontado(model):
         return any(getattr(field, 'related_model', None) == apontado
                    for field in model._meta.get_fields())
 
     return [model for app in appconfs for model in app.models.values()
-            if tem_referencia_a_autor(model)]
+            if tem_referencia_a_apontado(model)]
