41de3c4fb587f5db52a1b26c7e355c79a2e09608
João Pedro Sconetto <sconetto.joao@gmail.com>
2018-04-16 10:17:07 -0300

    Fix #1874

--- sapl/sessao/views.py
+++ sapl/sessao/views.py
@@ -1,4 +1,5 @@
 from re import sub
+from operator import itemgetter
 
 from django.contrib import messages
 from django.contrib.auth.decorators import permission_required
@@ -988,14 +989,16 @@ class MesaView(FormMixin, DetailView):
                 [p.parlamentar for p in parlamentares]) - set(
                 parlamentares_ocupados))
 
+        org_parlamentares_vagos = sorted(parlamentares_vagos, key=lambda x: x.nome_completo)
+
         # Se todos os cargos estiverem ocupados, a listagem de parlamentares
         # deve ser renderizada vazia
         if not cargos_vagos:
-            parlamentares_vagos = []
+            org_parlamentares_vagos = []
 
         context.update(
             {'composicao_mesa': mesa,
-             'parlamentares': parlamentares_vagos,
+             'parlamentares': org_parlamentares_vagos,
              'cargos_vagos': cargos_vagos})
 
         return self.render_to_response(context)
