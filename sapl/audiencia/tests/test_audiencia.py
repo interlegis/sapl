e2de5de8c250cbe8e9647dee42ec1b7b41db44b3
Mariana Mendes <mare.s.mendes@gmail.com>
2018-07-06 07:56:35 -0300

    Adiciona teste de audiencia

--- sapl/audiencia/tests/test_audiencia.py
+++ sapl/audiencia/tests/test_audiencia.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+import pytest
+from django.utils.translation import ugettext as _
+from model_mommy import mommy
 
-# Create your tests here.
+from sapl.audiencia import forms
+
+@pytest.mark.django_db(transaction=False)
+def test_valida_campos_obrigatorios_audiencia_form():
+    form = forms.AudienciaForm(data={})
+
+    assert not form.is_valid()
+
+    errors = form.errors
+
+    assert errors['nome'] == [_('Este campo é obrigatório.')]
+    assert errors['tema'] == [_('Este campo é obrigatório.')]
+    assert errors['tipo'] == [_('Este campo é obrigatório.')]
+    assert errors['tipo_materia'] == [_('Este campo é obrigatório.')]
+    assert errors['numero_materia'] == [_('Este campo é obrigatório.')]
+    assert errors['ano_materia'] == [_('Este campo é obrigatório.')]
+    assert errors['data'] == [_('Este campo é obrigatório.')]
+    assert errors['hora_inicio'] == [_('Este campo é obrigatório.')]
+    assert errors['hora_fim'] == [_('Este campo é obrigatório.')]
+
+    assert len(errors) == 9
