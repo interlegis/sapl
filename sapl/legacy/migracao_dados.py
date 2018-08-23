3b52eb641b6e40c19834feb660aa4f4016fdfa40
Marcio Mazza <marciomazza@gmail.com>
2018-08-23 10:27:55 -0300

    Propaga exclusões de matéria antes de migrar

--- sapl/legacy/migracao_dados.py
+++ sapl/legacy/migracao_dados.py
@@ -552,6 +552,8 @@ PROPAGACOES_DE_EXCLUSAO = [
     ('materia_legislativa', 'numeracao', 'cod_materia'),
     ('materia_legislativa', 'expediente_materia', 'cod_materia'),
     ('materia_legislativa', 'ordem_dia', 'cod_materia'),
+    ('materia_legislativa', 'acomp_materia', 'cod_materia'),
+    ('materia_legislativa', 'despacho_inicial', 'cod_materia'),
 
     # norma
     ('norma_juridica', 'vinculo_norma_juridica', 'cod_norma_referente'),
