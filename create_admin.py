6dd901d1d9a875754e1127cacbcd227534ba616b
Edward Ribeiro <eribeiro@interlegis.leg.br>
2018-08-29 17:23:26 -0300

    HOT-FIX: conserta bug introduzido em e73b0e2

--- create_admin.py
+++ create_admin.py
@@ -14,6 +14,7 @@ def get_enviroment_admin_password(username):
             "[SUPERUSER] Environment variable $ADMIN_PASSWORD"
             " for user %s was not set. Leaving..." % username)
         sys.exit('MISSING_ADMIN_PASSWORD')
+    return password
 
 
 def create_user_interlegis():
