b3e0cdc6ece2c4ec4988d97ca8716ed8f1a48801
Mariana Mendes <mare.s.mendes@gmail.com>
2018-08-30 17:11:42 -0300

    Fix #2180 (#2181)
    * Fix #2180
    
    * Update views.py
    
    Adiciona o object_list para ordenação

--- sapl/norma/views.py
+++ sapl/norma/views.py
@@ -86,6 +86,12 @@ class NormaPesquisaView(FilterView):
 
         context['title'] = _('Pesquisar Norma Jurídica')
 
+        self.filterset.form.fields['o'].label = _('Ordenação')
+        
+        qs = self.object_list
+        if 'o' in self.request.GET and not self.request.GET['o']:
+            qs = qs.order_by('-ano', 'tipo', '-numero')
+
         qr = self.request.GET.copy()
 
         if 'page' in qr:
