bbcdbda8196683abd95673371c9932f9196c412b
Edward Ribeiro <eribeiro@interlegis.leg.br>
2018-09-24 18:10:53 -0300

    HOT-FIX: remove setagem de observacao em ordemdia/expediente

--- sapl/sessao/views.py
+++ sapl/sessao/views.py
@@ -2759,9 +2759,6 @@ class AdicionarVariasMateriasExpediente(PermissionRequiredForAppCrudMixin,
                 expediente = ExpedienteMateria()
                 expediente.sessao_plenaria_id = self.kwargs['pk']
                 expediente.materia_id = materia.id
-                # TODO: o campo observacao deve ser uma copia de ML.ementa?
-                expediente.observacao = MateriaLegislativa.objects.get(
-                    pk=materia.id).ementa
                 if lista_materias_expediente:
                     posicao = lista_materias_expediente.last().numero_ordem + 1
                     expediente.numero_ordem = posicao
@@ -2831,9 +2828,6 @@ class AdicionarVariasMateriasOrdemDia(AdicionarVariasMateriasExpediente):
                 ordem_dia = OrdemDia()
                 ordem_dia.sessao_plenaria_id = self.kwargs['pk']
                 ordem_dia.materia_id = materia.id
-                # TODO: o campo observacao deve ser uma copia de ML.ementa?
-                ordem_dia.observacao = MateriaLegislativa.objects.get(
-                    pk=materia.id).ementa
                 if lista_materias_ordem_dia:
                     posicao = lista_materias_ordem_dia.last().numero_ordem + 1
                     ordem_dia.numero_ordem = posicao
