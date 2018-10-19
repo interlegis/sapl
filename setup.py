4c05f0dee7740de474daf5ccbb54472fd15e535a
VictorFabreF <victorfabre@me.com>
2018-10-19 12:44:38 -0300

    bump version

--- setup.py
+++ setup.py
@@ -11,29 +11,30 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 install_requires = [
     'dj-database-url==0.4.1',
     'django-haystack==2.6.0',
-    'django>=1.9,<1.10',
+    'django>=1.10,<1.11',
     # TODO O django-admin-bootstrapped 2.5.7 não inseriu a mudança que permite
     # a compatibilidade com Django 1.9+. A linha abaixo será mudada quando uma
     # nova versão do django-admin-bootstrapped for lançada
     # 'git+git://github.com/django-admin-bootstrapped/
     #      django-admin-bootstrapped.git',
     'django-bootstrap3==7.0.1',
-    'django-bower==5.1.0',
+    'django-bower==5.2.0',
     'django-braces==1.9.0',
     'django-compressor==2.0',
-    'django-crispy-forms==1.6.0',
-    'django-extensions==1.6.7',
-    'django-extra-views==0.8.0',
+    'django-crispy-forms==1.6.1',
+    'django-extensions==1.9.8',
+    'django-extra-views==0.11.0',
     'django-filter==0.15.3',
     'django-floppyforms==1.6.2',
-    'django-model-utils==2.5',
-    'django-sass-processor==0.5.4',
-    'djangorestframework',
+    'django-model-utils==3.1.1',
+    'django-sass-processor==0.5.8',
+    'djangorestframework==3.4.0',
     'drfdocs',
-    'easy-thumbnails==2.3',
+    'easy-thumbnails==2.5',
+    'django-image-cropping==1.1.0',
     # 'git+git://github.com/interlegis/trml2pdf.git',
     'libsass==0.11.1',
-    'psycopg2==2.7.3',
+    'psycopg2==2.7.4',
     'python-decouple==3.0',
     'pytz==2016.4',
     'pyyaml==3.11',
@@ -45,11 +46,13 @@ install_requires = [
     'gunicorn==19.6.0',
     'django-reversion==2.0.8',
     'WeasyPrint==0.42',
-    'whoosh==2.7.4'
+    'whoosh==2.7.4',
+    'django-speedinfo==1.3.5',
+    'django-reversion-compare==0.8.4'
 ]
 setup(
     name='interlegis-sapl',
-    version='3.1.121-BETA',
+    version='3.1.127',
     packages=find_packages(),
     include_package_data=True,
     license='GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007',
