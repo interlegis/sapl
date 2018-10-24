145d5c1844b3a6f1106b7c77a606d1706fb3cc90
Cesar Carvalho <cesaraugc@gmail.com>
2018-10-24 16:48:35 -0300

    FIX #2328

--- sapl/protocoloadm/views.py
+++ sapl/protocoloadm/views.py
@@ -772,11 +772,12 @@ class PesquisarDocumentoAdministrativoView(DocumentoAdministrativoMixin,
 
         self.filterset.form.fields['o'].label = _('Ordenação')
         
-        
-        if request.user.has_perm(permission_required):
-            length = self.object_list.count()
-        else:
+        # é usada essa verificação anônima para quando os documentos administrativos 
+        # estão no modo ostensivo, mas podem existir documentos administrativos restritos
+        if request.user.is_anonymous():
             length = self.object_list.filter(restrito=False).count()
+        else:
+            length = self.object_list.count()
         
         context = self.get_context_data(filter=self.filterset,
                                         filter_url=url,
