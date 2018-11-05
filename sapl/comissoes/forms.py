3a6cc6810b1fa680b10b57dc97da161dda372dc6
Cesar Augusto de Carvalho <cesaraugc@gmail.com>
2018-11-05 08:37:31 -0300

    Fix Chamado 676634 (osTicket) (#2348)
    LGTM.

--- sapl/comissoes/forms.py
+++ sapl/comissoes/forms.py
@@ -33,6 +33,7 @@ class ComposicaoForm(forms.ModelForm):
 
         periodo = cleaned_data['periodo']
         comissao_pk = self.initial['comissao'].id
+        cleaned_data['comissao'] = self.initial['comissao']
         intersecao_periodo = Composicao.objects.filter(
             Q(periodo__data_inicio__lte=periodo.data_fim,
                 periodo__data_fim__gte=periodo.data_fim) |
