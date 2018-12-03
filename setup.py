ccfd5203cc1b9f4ef4fb4e26d3873db572af302c
Edward Ribeiro <eribeiro@interlegis.leg.br>
2018-12-03 16:54:59 -0200

    Release: 3.1.137

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ install_requires = [
 ]
 setup(
     name='interlegis-sapl',
-    version='3.1.136',
+    version='3.1.137',
     packages=find_packages(),
     include_package_data=True,
     license='GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007',
