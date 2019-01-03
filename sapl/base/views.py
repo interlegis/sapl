f3eb68fe64f15c43272fb90bccfe493d9268f51e
João Rodrigues <joaomatheus.152013@gmail.com>
2019-01-03 17:57:57 -0200

    Alterar a apresentação dos resultados

--- sapl/base/views.py
+++ sapl/base/views.py
@@ -1479,7 +1479,7 @@ def bancada_comissao_autor_externo():
 
     tipo_autor_externo = TipoAutor.objects.filter(descricao='Externo')
 
-    for bancada in Bancada.objects.all():
+    for bancada in Bancada.objects.all().order_by('nome'):
         autor_externo = bancada.autor.filter(tipo=tipo_autor_externo)
 
         # if len(autor_externo) == 1:
@@ -1497,7 +1497,7 @@ def bancada_comissao_autor_externo():
         #            (q_autor_externo, bancada, 'Bancada')
         #        )
 
-    for comissao in Comissao.objects.all():
+    for comissao in Comissao.objects.all().order_by('nome'):
         autor_externo = comissao.autor.filter(tipo=tipo_autor_externo)
 
         # if len(autor_externo) == 1:
@@ -1623,7 +1623,7 @@ class ListarParlMandatosIntersecaoView(PermissionRequiredMixin, ListView):
 
 def materias_protocolo_inexistente():
     materias = []
-    for materia in MateriaLegislativa.objects.all().order_by('-ano'):
+    for materia in MateriaLegislativa.objects.order_by('-ano', 'numero'):
         if materia.numero_protocolo:
             exists = Protocolo.objects.filter(
                 ano=materia.ano, numero=materia.numero_protocolo).exists()
@@ -1659,7 +1659,7 @@ class ListarMatProtocoloInexistenteView(PermissionRequiredMixin, ListView):
 
 def protocolos_materias():
     protocolos = []
-    for protocolo in Protocolo.objects.all():
+    for protocolo in Protocolo.objects.order_by('-ano', 'numero'):
         materias_protocolo = MateriaLegislativa.objects.filter(
             ano=protocolo.ano, numero_protocolo=protocolo.numero)
         if len(materias_protocolo) > 1:
@@ -1692,7 +1692,7 @@ class ListarProtocolosMateriasView(PermissionRequiredMixin, ListView):
 
 def protocolos_duplicados():
     protocolos = {}
-    for p in Protocolo.objects.all():
+    for p in Protocolo.objects.order_by('-ano', 'numero'):
         key = "{}/{}".format(p.numero, p.ano)
         val = protocolos.get(key, list())
         val.append(p)
