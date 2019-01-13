fc604679b95387f8f93d9427f89737f691958aba
Leandro Roberto <leandroroberto.br@gmail.com>
2019-01-13 01:07:48 -0200

    add vue.config.js e configura comunicação com webpack-loader

---
+++ sapl-frontend/vue.config.js
@@ -0,0 +1,30 @@
+const BundleTracker = require('webpack-bundle-tracker')
+
+module.exports = {
+  publicPath: 'http://localhost:8080/',
+  outputDir: './dist/',
+  
+
+  chainWebpack: config => {
+
+    config.optimization
+      .splitChunks(false)
+
+    config
+      .plugin('BundleTracker')
+      .use(BundleTracker, [{ filename: './webpack-stats.json' }])
+
+    config.resolve.alias
+      .set('__STATIC__', 'static')
+
+    config.devServer
+      .public('')
+      .host('localhost')
+      .port(8080)
+      .hotOnly(true)
+      .watchOptions({ poll: 1000 })
+      .https(false)
+      .headers({ 'Access-Control-Allow-Origin': '\*' })
+
+  }
+}
