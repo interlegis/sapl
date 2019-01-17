b9e0cd14725833a62e6905c5567e03060238dac0
Leandro Roberto <leandroroberto.br@gmail.com>
2019-01-16 23:30:27 -0200

    remove código antigo do jquery-mask-plugin

--- sapl/static/js/app.js
+++ sapl/static/js/app.js
@@ -38,18 +38,6 @@ function refreshDatePicker() {
     $('.dateinput').datepicker();
 }
 
-function refreshMask() {
-    $('.telefone').mask("(99) 9999-9999", {placeholder:"(__) ____ -____"});
-    $('.cpf').mask("000.000.000-00", {placeholder:"___.___.___-__"});
-    $('.cep').mask("00000-000", {placeholder:"_____-___"});
-    $('.rg').mask("0.000.000", {placeholder:"_.___.___"});
-    $('.titulo_eleitor').mask("0000.0000.0000.0000", {placeholder:"____.____.____.____"});
-    $('.dateinput').mask('00/00/0000', {placeholder:"__/__/____"});
-    $('.hora').mask("00:00", {placeholder:"hh:mm"});
-    $('.hora_hms').mask("00:00:00", {placeholder:"hh:mm:ss"});
-    $('.timeinput').mask("00:00:00", {placeholder:"hh:mm:ss"});
-    $('.cronometro').mask("00:00:00", {placeholder:"hh:mm:ss"});
-}
 
 function OptionalCustomFrontEnd() {
     // Adaptações opcionais de layout com a presença de JS.
@@ -124,7 +112,6 @@ function OptionalCustomFrontEnd() {
 
 $(document).ready(function(){
     refreshDatePicker();
-    refreshMask();
     initTinymce("texto-rico");
 
     //OptionalCustomFrontEnd();
