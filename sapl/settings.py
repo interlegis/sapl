8c8862bb9dedc73be9602725a3f935ee40f03211
Leandro Roberto <leandroroberto.br@gmail.com>
2019-02-11 16:16:25 -0200

    retira browsable renderer

--- sapl/settings.py
+++ sapl/settings.py
@@ -160,7 +160,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "sapl.api.permissions.SaplModelPermissions",
