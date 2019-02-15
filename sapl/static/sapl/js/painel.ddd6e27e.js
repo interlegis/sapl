578ff757a76f10789f558548e8926cfd46484d62
Leandro Roberto <leandroroberto.br@gmail.com>
2019-02-14 23:22:58 -0200

    torna sapl + sapl-frontend instalavel em outro projeto

--- sapl/static/sapl/js/painel.ddd6e27e.js
+++ sapl/static/sapl/js/painel.ddd6e27e.js
@@ -136,7 +136,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/static/";
+/******/ 	__webpack_require__.p = "/static/sapl/";
 /******/
 /******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
 /******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
