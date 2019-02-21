ae900ad0eaa63b1c5166033089a719fbc762b80b
Vinícius Cantuária <cantuariavc@gmail.com>
2019-02-21 16:56:34 -0300

    Adicionar comissões que estiverem ativas em unidade destino de tramitação (#2530)
    * Corrige adição de unidade destino de tramitação
    
    * Update forms.py

--- sapl/materia/forms.py
+++ sapl/materia/forms.py
@@ -424,6 +424,11 @@ class TramitacaoForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(TramitacaoForm, self).__init__(*args, **kwargs)
         self.fields['data_tramitacao'].initial = timezone.now().date()
+        ust = UnidadeTramitacao.objects.select_related().all()
+        unidade_tramitacao_destino = [(ut.pk, ut) for ut in ust if ut.comissao and ut.comissao.ativa]
+        unidade_tramitacao_destino.extend([(ut.pk, ut) for ut in ust if ut.orgao])
+        unidade_tramitacao_destino.extend([(ut.pk, ut) for ut in ust if ut.parlamentar])
+        self.fields['unidade_tramitacao_destino'].choices = unidade_tramitacao_destino
 
     def clean(self):
         super(TramitacaoForm, self).clean()
