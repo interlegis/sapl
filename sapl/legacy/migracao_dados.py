37b284412ed70f5087123a847f28f914aebc0c52
Marcio Mazza <marciomazza@gmail.com>
2019-02-26 09:54:11 -0300

    Removed unnecessary code (assuming python 3.6)

--- sapl/legacy/migracao_dados.py
+++ sapl/legacy/migracao_dados.py
@@ -53,20 +53,6 @@ from sapl.utils import normalize
 from .scripts.normaliza_dump_mysql import normaliza_dump_mysql
 
 
-# YAML SETUP  ###############################################################
-def dict_representer(dumper, data):
-    return dumper.represent_dict(data.items())
-
-yaml.add_representer(OrderedDict, dict_representer)
-
-
-# importante para preservar a ordem ao ler yaml no python 3.5
-def dict_constructor(loader, node):
-    return OrderedDict(loader.construct_pairs(node))
-
-yaml.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-                     dict_constructor)
-
 # BASE ######################################################################
 #  apps to be migrated, in app dependency order (very important)
 appconfs = [apps.get_app_config(n) for n in [
