8bcb04d1f81098c5d9460b3722bcb4f69b6ce502
ulyssesBML <ulysses3353@gmail.com>
2019-02-28 17:36:43 -0300

    Fix #2552

--- sapl/lexml/views.py
+++ sapl/lexml/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from sapl.crud.base import CrudAux
 from sapl.lexml.OAIServer import OAIServerFactory, get_config
@@ -16,3 +17,6 @@ def lexml_request(request):
                                   'metadataPrefix': request.GET.get('metadataPrefix', 'oai_lexml')})
     response = r.decode('UTF-8')
     return HttpResponse(response, content_type='text/xml')
+
+def request_search(request, keyword):
+    return render(request,"lexml/resultado-pesquisa.html",{"keyword":keyword})
