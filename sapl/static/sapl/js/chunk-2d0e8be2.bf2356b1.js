903fbe3d35f700c016f6c7ccb49f298279277497
Leandro Roberto <leandroroberto.br@gmail.com>
2019-03-01 16:41:59 -0300

    recompila frontend com ult update sapl-frontend/master

--- sapl/static/sapl/js/chunk-2d0e8be2.bf2356b1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["chunk-2d0e8be2"],{"8b24":function(n,e,t){"use strict";t.r(e);var a={name:"index",data:function(){return{}}},i=t("0c7c"),c=Object(i.a)(a,function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("\n    teste de página para dentro do main - INDEX INDEX\n")])},[],!1,null,null,null);e.default=c.exports}}]);
