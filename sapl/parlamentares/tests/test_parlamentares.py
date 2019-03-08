3937a3c6fcee1c05217d96dfbc384802e37ea9a9
Vinícius Cantuária <cantuariavc@gmail.com>
2019-03-08 15:29:27 -0300

    Reverte separação de criação da vinculação do parlamentar (#2593)
    * Revert "Alterando estilo do botao de pesquisar parlamentares (#2573)"
    
    This reverts commit 4fbe8a62d5ab69d304cccfb5a904d95190b28043.
    
    * Revert "HOT-FIX - remoçao de block.super desnecessario"
    
    This reverts commit 3223f3516086ba7782f7a1a4af9d87cde534c260.
    
    * Revert "Fix #2536 - Separa criação de mandato e parlamentar (#2551)"
    
    This reverts commit 0c7c17de658397a5ea3ec9b75addf9f4fb772229.
    
    * Altera localização de pesquisar parlamentar

--- sapl/parlamentares/tests/test_parlamentares.py
+++ sapl/parlamentares/tests/test_parlamentares.py
@@ -20,14 +20,21 @@ def test_cadastro_parlamentar(admin_client):
     response = admin_client.post(url, {'nome_completo': 'Teresa Barbosa',
                                        'nome_parlamentar': 'Terezinha',
                                        'sexo': 'F',
-                                       'ativo': 'True'
-                                       },
+                                       'ativo': 'True',
+                                       'legislatura': legislatura.id,
+                                       'data_expedicao_diploma': '2001-01-01'},
                                  follow=True)
 
     [parlamentar] = Parlamentar.objects.all()
     assert parlamentar.nome_parlamentar == 'Terezinha'
     assert parlamentar.sexo == 'F'
     assert parlamentar.ativo is True
+    # o primeiro mandato é criado
+    [mandato] = Mandato.objects.all()
+    assert mandato.parlamentar == parlamentar
+    assert str(mandato.data_expedicao_diploma) == '2001-01-01'
+    assert mandato.legislatura == legislatura
+    assert mandato.data_fim_mandato == legislatura.data_fim
 
 
 @pytest.mark.django_db(transaction=False)
@@ -35,7 +42,9 @@ def test_incluir_parlamentar_errors(admin_client):
     url = reverse('sapl.parlamentares:parlamentar_create')
     response = admin_client.post(url)
     erros_esperados = {campo: ['Este campo é obrigatório.']
-                       for campo in ['nome_parlamentar',
+                       for campo in ['legislatura',
+                                     'data_expedicao_diploma',
+                                     'nome_parlamentar',
                                      'nome_completo',
                                      'sexo',
                                      ]}
