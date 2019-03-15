ed79e7e7a0dbd92ab9b816a1ccc50e96572cbf5f
Leandro Roberto <leandroroberto.br@gmail.com>
2019-03-14 22:50:12 -0300

    nova release do sapl-frontend para master

---
+++ sapl/static/sapl/frontend/js/chunk-2d0c4a82.8c82f4d6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["chunk-2d0c4a82"],{"3c84":function(e,n,s){"use strict";s.r(n);var a={name:"sessao-plenaria-module"},t=s("0c7c"),c=Object(t.a)(a,function(){var e=this.$createElement,n=this._self._c||e;return n("div",{staticClass:"online-sessaolenaria"},[n("router-view")],1)},[],!1,null,null,null);n.default=c.exports}}]);
