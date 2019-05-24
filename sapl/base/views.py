9a60367ee2b5a9b4ba1ae457246777a5164a9ce6
Cesar Augusto de Carvalho <cesaraugc@gmail.com>
2019-05-24 12:47:51 -0300

    Fix #2794 - Relatório de tramitações (#2795)
    * Fix relatorio de tramitacoes
    
    * Refatora relatorio de Historico Tramitacao
    
    * Corrige relatorio de fim prazo tramitacao

--- sapl/base/views.py
+++ sapl/base/views.py
@@ -319,11 +319,8 @@ class RelatoriosListView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(TemplateView, self).get_context_data(**kwargs)
         estatisticas_acesso_normas = AppConfig.objects.first().estatisticas_acesso_normas
-        if estatisticas_acesso_normas == 'S':
-            context['estatisticas_acesso_normas'] = True
-        else:
-            context['estatisticas_acesso_normas'] = False
-
+        context['estatisticas_acesso_normas'] = True if estatisticas_acesso_normas == 'S' else False
+        
         return context
 
 
@@ -475,7 +472,7 @@ class RelatorioHistoricoTramitacaoView(FilterView):
     def get_context_data(self, **kwargs):
         context = super(RelatorioHistoricoTramitacaoView,
                         self).get_context_data(**kwargs)
-        context['title'] = _('Histórico de Tramitações')
+        context['title'] = _('Histórico de Tramitações de Matérias Legislativas')
         if not self.filterset.form.is_valid():
             return context
         qr = self.request.GET.copy()
@@ -490,16 +487,25 @@ class RelatorioHistoricoTramitacaoView(FilterView):
                 str(TipoMateriaLegislativa.objects.get(id=tipo)))
         else:
             context['tipo'] = ''
+
         if self.request.GET['tramitacao__status']:
             tramitacao_status = self.request.GET['tramitacao__status']
             context['tramitacao__status'] = (
                 str(StatusTramitacao.objects.get(id=tramitacao_status)))
         else:
             context['tramitacao__status'] = ''
+
         if self.request.GET['tramitacao__unidade_tramitacao_local']:
             context['tramitacao__unidade_tramitacao_local'] = \
                 (str(UnidadeTramitacao.objects.get(
                     id=self.request.GET['tramitacao__unidade_tramitacao_local'])))
+        else:
+            context['tramitacao__unidade_tramitacao_local'] = ''
+
+        if self.request.GET['tramitacao__unidade_tramitacao_destino']:
+            context['tramitacao__unidade_tramitacao_destino'] = \
+                (str(UnidadeTramitacao.objects.get(
+                    id=self.request.GET['tramitacao__unidade_tramitacao_destino'])))
         else:
             context['tramitacao__unidade_tramitacao_destino'] = ''
 
@@ -530,16 +536,25 @@ class RelatorioDataFimPrazoTramitacaoView(FilterView):
                 str(TipoMateriaLegislativa.objects.get(id=tipo)))
         else:
             context['tipo'] = ''
+
         if self.request.GET['tramitacao__status']:
             tramitacao_status = self.request.GET['tramitacao__status']
             context['tramitacao__status'] = (
                 str(StatusTramitacao.objects.get(id=tramitacao_status)))
         else:
             context['tramitacao__status'] = ''
+
         if self.request.GET['tramitacao__unidade_tramitacao_local']:
             context['tramitacao__unidade_tramitacao_local'] = \
                 (str(UnidadeTramitacao.objects.get(
                     id=self.request.GET['tramitacao__unidade_tramitacao_local'])))
+        else:
+            context['tramitacao__unidade_tramitacao_local'] = ''
+
+        if self.request.GET['tramitacao__unidade_tramitacao_destino']:
+            context['tramitacao__unidade_tramitacao_destino'] = \
+                (str(UnidadeTramitacao.objects.get(
+                    id=self.request.GET['tramitacao__unidade_tramitacao_destino'])))
         else:
             context['tramitacao__unidade_tramitacao_destino'] = ''
 
