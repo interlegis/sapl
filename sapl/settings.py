8b183c9156a2ca52ce3c6e6c62482fdd1168bd38
Cesar Carvalho <cesaraugc@gmail.com>
2019-06-24 15:30:03 -0300

    Release: 3.1.158

--- sapl/settings.py
+++ sapl/settings.py
@@ -41,7 +41,7 @@ ALLOWED_HOSTS = ['*']
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/?next='
 
-SAPL_VERSION = '3.1.157'
+SAPL_VERSION = '3.1.158'
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
