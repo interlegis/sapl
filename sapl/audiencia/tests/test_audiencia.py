e33ea0acac83d637569e73667c2a3b09cc2ad07a
João Rodrigues <cont.joaorodrigues@gmail.com>
2019-06-26 13:58:47 -0300

    Adiciona Testes (#2834)
    * Adicionar testes
    
    * Adicionar testes - Inconsistências de Dados
    
    * Adicionar testes - Inconsistências de Dados
    
    * Adicionar testes - Inconsistências de Dados
    
    * Retirar imports não utilizados

--- sapl/audiencia/tests/test_audiencia.py
+++ sapl/audiencia/tests/test_audiencia.py
@@ -1,10 +1,61 @@
 import pytest
-from django.utils.translation import ugettext as _
+import datetime
 from model_mommy import mommy
+from django.utils.translation import ugettext as _
 
 from sapl.audiencia import forms
-from sapl.audiencia.models import TipoAudienciaPublica
+from sapl.audiencia.models import AnexoAudienciaPublica
+from sapl.audiencia.models import TipoAudienciaPublica, AudienciaPublica
 from sapl.materia.models import MateriaLegislativa, TipoMateriaLegislativa
+
+
+@pytest.mark.django_db(transaction=False)
+def test_tipo_audiencia_publica_model():
+    mommy.make(TipoAudienciaPublica,
+               nome='Teste_Nome_Tipo_Audiencia_Publica',
+               tipo='A')
+
+    tipo_audiencia_publica = TipoAudienciaPublica.objects.first()
+    assert tipo_audiencia_publica.nome == 'Teste_Nome_Tipo_Audiencia_Publica'
+    assert tipo_audiencia_publica.tipo == 'A'
+
+
+@pytest.mark.django_db(transaction=False)
+def test_audiencia_publica_model():
+    mommy.make(AudienciaPublica,
+               numero=1,
+               nome='Teste_Nome_Audiencia_Publica',
+               tema='Teste_Tema_Audiencia_Publica',
+               data='2016-03-21',
+               hora_inicio='16:03')
+
+    audiencia_publica = AudienciaPublica.objects.first()
+
+    data = '2016-03-21'
+    teste_data = datetime.datetime.strptime(data, "%Y-%m-%d").date()
+
+    assert audiencia_publica.numero == 1
+    assert audiencia_publica.nome == 'Teste_Nome_Audiencia_Publica'
+    assert audiencia_publica.tema == 'Teste_Tema_Audiencia_Publica'
+    assert audiencia_publica.data == teste_data
+    assert audiencia_publica.hora_inicio == '16:03'
+
+
+@pytest.mark.django_db(transaction=False)
+def test_anexo_audiencia_publica_model():
+    audiencia = mommy.make(AudienciaPublica,
+                           numero=2,
+                           nome='Nome_Audiencia_Publica',
+                           tema='Tema_Audiencia_Publica',
+                           data='2017-04-22',
+                           hora_inicio='17:04')
+
+    mommy.make(AnexoAudienciaPublica,
+               audiencia=audiencia)
+
+    anexo_audiencia_publica = AnexoAudienciaPublica.objects.first()
+    assert anexo_audiencia_publica.audiencia == audiencia
+
 
 @pytest.mark.django_db(transaction=False)
 def test_valida_campos_obrigatorios_audiencia_form():
@@ -35,7 +86,5 @@ def test_audiencia_form_hora_invalida():
                                      'data': '2016-10-01',
                                      'hora_inicio': '10:00',
                                      'hora_fim': '9:00',
-                                  	})
+                                     })
     assert not form.is_valid()
-
-  
