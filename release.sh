3497eeb7c0e00be1d7962814d693b3476498e838
Edward Ribeiro <eribeiro@interlegis.leg.br>
2019-07-31 15:09:14 -0300

    Mudanças em release.sh

--- release.sh
+++ release.sh
@@ -72,19 +72,21 @@ function commit_and_push {
    git commit -m "Release: $FINAL_VERSION"
    git tag $FINAL_VERSION
 
-   echo "sending to github..."
-   git push origin
-   git push origin $FINAL_VERSION
+   echo "Para enviar pro github execute..."
+   echo "git push origin 3.1.x"
+   echo "git push origin "$FINAL_VERSION
 
    echo "done."
 }
 
 case "$1" in
     --latest)
+       git fetch
        echo $LATEST_VERSION
        exit 0
        ;;
     --major)
+       git fetch
        set_major_version
        echo "generating major release: "$FINAL_VERSION
        # git tag $FINAL_VERSION
@@ -93,6 +95,7 @@ case "$1" in
        exit 0
        ;;
     --rc)
+       git fetch
        set_rc_version
        echo "generating release candidate: "$FINAL_VERSION
        # git tag $FINAL_VERSION
