4b28e73937480f82f4496acad215aebde0168628
Cesar Carvalho <cesaraugc@gmail.com>
2019-08-08 12:24:56 -0300

    HOT-FIX: Adiciona import de logger em form de parlamentares

--- sapl/parlamentares/forms.py
+++ sapl/parlamentares/forms.py
@@ -237,6 +237,8 @@ class ParlamentarFilterSet(django_filters.FilterSet):
 
 class ParlamentarCreateForm(ParlamentarForm):
 
+    logger = logging.getLogger(__name__)
+
     class Meta(ParlamentarForm.Meta):
         widgets = {
             'fotografia': forms.ClearableFileInput(),
