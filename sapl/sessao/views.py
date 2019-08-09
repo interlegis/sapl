b718ce27efa67c577db3831463dc31d53b0c70f9
Cesar Carvalho <cesaraugc@gmail.com>
2019-08-09 13:21:09 -0300

    Fix #2920 - Adiciona campo de ordenação em TipoExpediente

--- sapl/sessao/views.py
+++ sapl/sessao/views.py
@@ -2087,16 +2087,16 @@ class ExpedienteView(FormMixin, DetailView):
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
         context = self.get_context_data(object=self.object)
-        tipos = TipoExpediente.objects.all().order_by('nome')
+        tipos = TipoExpediente.objects.all().order_by('ordenacao')
         expedientes_sessao = ExpedienteSessao.objects.filter(
-            sessao_plenaria_id=self.object.id).order_by('tipo__nome')
+            sessao_plenaria_id=self.object.id).order_by('tipo__ordenacao')
 
         expedientes_salvos = []
         for e in expedientes_sessao:
             expedientes_salvos.append(e.tipo)
 
         tipos_null = list(set(tipos) - set(expedientes_salvos))
-        tipos_null.sort(key=lambda x: x.nome)
+        tipos_null.sort(key=lambda x: x.ordenacao)
 
         expedientes = []
         for e, t in zip(expedientes_sessao, tipos):
