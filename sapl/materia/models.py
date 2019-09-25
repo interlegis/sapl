55c070b5776f6cb331a5f9be3fd9f8a2f7a575c1
João Rodrigues <cont.joaorodrigues@gmail.com>
2019-09-25 15:57:30 -0300

    Fix Ticket #923744 - Tempo de resposta do relatório de matérias em tramitação (#2955)
    * Otimiza relatório de matérias em tramitação
    
    * Refatoração
    
    * HOT-FIX: corrige erro apontado pelo travis
    
    * Adicionar nova model
    
    * Adicionar on_delete nos campos da model
    
    * Acelera pesquisa do relatório
    
    * Refatoração

--- sapl/materia/models.py
+++ sapl/materia/models.py
@@ -1084,3 +1084,15 @@ class Tramitacao(models.Model):
             'materia': self.materia,
             'status': self.status,
             'data': self.data_tramitacao.strftime("%d/%m/%Y")}
+
+
+class MateriaEmTramitacao(models.Model):
+    materia = models.ForeignKey(MateriaLegislativa, on_delete=models.DO_NOTHING)
+    tramitacao = models.ForeignKey(Tramitacao, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = "materia_materiaemtramitacao"
+
+    def __str__(self):
+        return '{}/{}'.format(self.materia, self.tramitacao)
