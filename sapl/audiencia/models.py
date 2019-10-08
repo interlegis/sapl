7a80cf92abc34b24332dae57a80fe096f537534e
João Rodrigues <cont.joaorodrigues@gmail.com>
2019-10-08 12:05:51 -0300

    Fix #2961 - AudienciaPublica

--- sapl/audiencia/models.py
+++ sapl/audiencia/models.py
@@ -119,17 +119,22 @@ class AudienciaPublica(models.Model):
         return self.nome
 
     def delete(self, using=None, keep_parents=False):
-        if self.upload_pauta:
-            self.upload_pauta.delete()
+        upload_pauta = self.upload_pauta
+        upload_ata = self.upload_ata
+        upload_anexo = self.upload_anexo
 
-        if self.upload_ata:
-            self.upload_ata.delete()
+        result = super().delete(using=using, keep_parents=keep_parents)
 
-        if self.upload_anexo:
-            self.upload_anexo.delete()
+        if upload_pauta:
+            upload_pauta.delete(save=False)
 
-        return models.Model.delete(
-            self, using=using, keep_parents=keep_parents)
+        if upload_ata:
+            upload_ata.delete(save=False)
+
+        if upload_anexo:
+            upload_anexo.delete(save=False)
+
+        return result
 
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
