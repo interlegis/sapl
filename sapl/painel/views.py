d3c5015057c221fb96b7972c2cdf28b72d1eb62f
ulyssesBML <ulysses3353@gmail.com>
2019-12-19 12:37:55 -0300

    Fix #3007 - Adcionando mudança de apresentação de oradores dependendo da materia

--- sapl/painel/views.py
+++ sapl/painel/views.py
@@ -21,7 +21,7 @@ from sapl.parlamentares.models import Legislatura, Parlamentar, Votante
 from sapl.sessao.models import (ExpedienteMateria, OradorExpediente, OrdemDia,
                                 PresencaOrdemDia, RegistroVotacao,
                                 SessaoPlenaria, SessaoPlenariaPresenca,
-                                VotoParlamentar, RegistroLeitura)
+                                VotoParlamentar, RegistroLeitura, OradorOrdemDia)
 from sapl.utils import filiacao_data, get_client_ip, sort_lista_chave
 
 from .models import Cronometro
@@ -368,17 +368,21 @@ def get_presentes(pk, response, materia):
     sessao = SessaoPlenaria.objects.get(id=pk)
     num_presentes = len(presentes)
     data_sessao = sessao.data_inicio
-    oradores = OradorExpediente.objects.filter(
-        sessao_plenaria_id=pk).order_by('numero_ordem')
-
+    
+    if type(materia) is ExpedienteMateria:
+        oradores = OradorExpediente.objects.filter(
+            sessao_plenaria_id=pk).order_by('numero_ordem')
+    else:
+        oradores = OradorOrdemDia.objects.filter(
+            sessao_plenaria_id=pk).order_by('numero_ordem')
+    
     oradores_list = []
     for o in oradores:
-
         oradores_list.append(
             {
                 'nome': o.parlamentar.nome_parlamentar,
                 'numero': o.numero_ordem
-            })
+            })        
 
     presentes_list = []
     for p in presentes:
