e9e3770f989c106dee3b1706079489f309746214
João Rodrigues <cont.joaorodrigues@gmail.com>
2020-04-09 01:44:41 -0300

    Adiciona informações na tela de videoconferência

--- sapl/sdr/urls.py
+++ sapl/sdr/urls.py
@@ -1,13 +1,20 @@
 
 from .apps import AppConfig
-from .views import ChatView, DeliberacaoRemotaCrud
+from .views import (ChatView,
+                    DeliberacaoRemotaCrud,
+                    get_dados_deliberacao_remota)
+
 from django.conf.urls import include, url
 
 app_name = AppConfig.name
 
 urlpatterns = [
+    url(r'^sdr/',
+        include(DeliberacaoRemotaCrud.get_urls()),
+        name='deliberacaoremota'),
     url(r'^sdr/chat/(?P<pk>[0-9]+)$',
         ChatView.as_view(), name='chat-session'),
-    url(r'^sdr/',
-        include(DeliberacaoRemotaCrud.get_urls()), name='deliberacaoremota'),
+    url(r'^sdr/(?P<pk>\d+)/dados$',
+        get_dados_deliberacao_remota,
+        name='dados_deliberacao_remota'),
 ]
--- sapl/sdr/views.py
+++ sapl/sdr/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 from django.core.urlresolvers import reverse, reverse_lazy
+from django.http import JsonResponse
 from django.views.generic import TemplateView
 from django.utils import timezone
 
@@ -13,7 +14,10 @@ from sapl.parlamentares.models import Parlamentar
 from sapl.rules import RP_LIST, RP_DETAIL
 from sapl.sdr.forms import DeliberacaoRemotaForm
 from sapl.sdr.models import DeliberacaoRemota, gen_session_id
-from sapl.sessao.models import SessaoPlenaria
+from sapl.sessao.models import (ExpedienteMateria, OradorExpediente,
+                                OradorOrdemDia, OrdemDia, PresencaOrdemDia,
+                                RegistroLeitura, RegistroVotacao,
+                                SessaoPlenaria, SessaoPlenariaPresenca)
 
 
 class DeliberacaoRemotaCrud(Crud):
@@ -54,6 +58,110 @@ class DeliberacaoRemotaCrud(Crud):
             context['deliberacao'] = deliberacao
 
             return context
+
+
+#@user_passes_test(check_permission)
+def get_dados_deliberacao_remota(request, pk):
+    sessao = SessaoPlenaria.objects.get(id=pk)
+
+    sessao_plenaria_presenca, oradores_expediente, materias_expediente = [], [], []
+    ordemdia_presenca, oradores_ordemdia, materias_ordemdia = [], [], []
+
+    f_od = 1 if not ExpedienteMateria.objects.filter(sessao_plenaria=sessao, votacao_aberta=True).exists() else 0
+    f_em = 1 if not OrdemDia.objects.filter(sessao_plenaria=sessao, votacao_aberta=True).exists() else 0
+
+    if f_em:
+        for presenca in SessaoPlenariaPresenca.objects.filter(sessao_plenaria=sessao):
+            sessao_plenaria_presenca.append(presenca.parlamentar.nome_parlamentar)
+
+        for orador in OradorExpediente.objects.filter(sessao_plenaria=sessao).order_by("numero_ordem"):
+            oradores_expediente.append(str(orador.numero_ordem) + " - " + orador.parlamentar.nome_parlamentar)
+
+        for expediente in ExpedienteMateria.objects.filter(sessao_plenaria=sessao):
+            if expediente.tipo_votacao == 4:
+                if RegistroLeitura.objects.filter(expediente=expediente).exists():
+                    materias_expediente.append(
+                        (
+                            str(expediente.numero_ordem), expediente.materia.__str__(),
+                            expediente.materia.ementa, "Matéria Lida"
+                        )
+                    )
+                else:
+                    materias_expediente.append(
+                        (
+                            str(expediente.numero_ordem), expediente.materia.__str__(),
+                            expediente.materia.ementa, "Matéria Não Lida"
+                        )
+                    )   
+            else:
+                if RegistroVotacao.objects.filter(expediente=expediente).exists():
+                    registro = RegistroVotacao.objects.get(expediente=expediente)
+                    materias_expediente.append(
+                        (
+                            str(expediente.numero_ordem), expediente.materia.__str__(),
+                            expediente.materia.ementa, registro.tipo_resultado_votacao.nome
+                        )
+                    )
+                else:
+                    materias_expediente.append(
+                        (
+                            str(expediente.numero_ordem), expediente.materia.__str__(),
+                            expediente.materia.ementa, "Matéria Não Votada"
+                        )
+                    )
+
+    if f_od:
+        for presenca in PresencaOrdemDia.objects.filter(sessao_plenaria=sessao):
+            ordemdia_presenca.append(presenca.parlamentar.nome_parlamentar)
+
+        for orador in OradorOrdemDia.objects.filter(sessao_plenaria=sessao).order_by("numero_ordem"):
+            oradores_ordemdia.append(str(orador.numero_ordem) + " - " + orador.parlamentar.nome_parlamentar)
+
+        for ordemdia in OrdemDia.objects.filter(sessao_plenaria=sessao):
+            if ordemdia.tipo_votacao == 4:
+                if RegistroLeitura.objects.filter(ordem=ordemdia).exists():
+                    materias_ordemdia.append(
+                        (
+                            str(ordemdia.numero_ordem), ordemdia.materia.__str__(),
+                            ordemdia.materia.ementa, "Matéria Lida"
+                        )
+                    )
+                else:
+                    materias_ordemdia.append(
+                        (
+                            str(ordemdia.numero_ordem), ordemdia.materia.__str__(),
+                            ordemdia.materia.ementa, "Matéria Não Lida"
+                        )
+                    )   
+            else:
+                if RegistroVotacao.objects.filter(ordem=ordemdia).exists():
+                    registro = RegistroVotacao.objects.get(ordem=ordemdia)
+                    materias_ordemdia.append(
+                        (
+                            str(ordemdia.numero_ordem), ordemdia.materia.__str__(),
+                            ordemdia.materia.ementa, registro.tipo_resultado_votacao.nome
+                        )
+                    )
+                else:
+                    materias_ordemdia.append(
+                        (
+                            str(ordemdia.numero_ordem), ordemdia.materia.__str__(),
+                            ordemdia.materia.ementa, "Matéria Não Votada"
+                        )
+                    )
+
+    response = {
+        'sessao_plenaria': str(sessao), 'sessao_plenaria_data': sessao.data_inicio.strftime('%d/%m/%Y'),
+        'sessao_plenaria_hora_inicio': sessao.hora_inicio,
+        'sessao_plenaria_iniciada': "Sim" if sessao.iniciada else "Não",
+        'f_em': f_em, 'f_od': f_od, 'sessao_plenaria_presenca': sessao_plenaria_presenca,
+        'oradores_expediente': oradores_expediente, 'materias_expediente': materias_expediente,
+        'ordemdia_presenca': ordemdia_presenca, 'oradores_ordemdia': oradores_ordemdia,
+        'materias_ordemdia': materias_ordemdia
+    }
+
+    return JsonResponse(response)
+
 
 class ChatView(TemplateView):
 # class ChatView(PermissionRequiredMixin, TemplateView):
