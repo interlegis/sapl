4d832b79f703d21504dc8a8740b1aac2088855ee
João Rodrigues <cont.joaorodrigues@gmail.com>
2020-05-13 18:37:58 -0300

    Fix parlamentares migrations (após cópia)

--- sapl/parlamentares/migrations/0028_auto_20190515_1744.py
+++ sapl/parlamentares/migrations/0028_auto_20190515_1744.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('parlamentares', '0026_bloco'),
+        ('parlamentares', '0027_auto_20190430_0839'),
     ]
 
     operations = [
