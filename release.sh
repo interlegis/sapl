8c37058dcf39c016aa5cd8c806e57460cd16a0a3
eribeiro <edwardr@senado.leg.br>
2020-09-09 15:00:57 -0300

    Refatora arquivos Docker

--- release.sh
+++ release.sh
@@ -30,11 +30,11 @@ FINAL_VERSION=
 
 function change_files {
 
-    OLD_VERSION=$(grep -E 'interlegis/sapl:'$VERSION_PATTERN docker-compose.yml | cut -d':' -f3)
+    OLD_VERSION=$(grep -E 'interlegis/sapl:'$VERSION_PATTERN docker/docker-compose.yml | cut -d':' -f3)
 
     echo "Atualizando de "$OLD_VERSION" para "$FINAL_VERSION
 
-    sed -E -i "s|$OLD_VERSION|$FINAL_VERSION|g" docker-compose.yml
+    sed -E -i "s|$OLD_VERSION|$FINAL_VERSION|g" docker/docker-compose.yml
 
     sed -E -i "s|$OLD_VERSION|$FINAL_VERSION|g" setup.py
 
