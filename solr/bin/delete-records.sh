9a2978253abba5da0eafa189e9f606d1d27fcd42
Vinícius Cantuária <cantuariavc@gmail.com>
2020-11-26 12:20:54 -0300

    Update and rename delete-records.sh to delete-all-documents.sh

--- solr/bin/delete-records.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-SOLR_URL=${SOLR_URL-'http://localhost:8983/solr'}
-SOLR_COLLECTION=${SOLR_COLLECTION-'sapl'}
-
-curl -X POST "$SOLR_URL/$SOLR_COLLECTION/update?commit=true&stream.body=<delete><query>*%3A*</query></delete>"
