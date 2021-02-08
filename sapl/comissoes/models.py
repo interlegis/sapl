09c8c191bc49f377f7feefebb1cd7c54767cd9d1
eribeiro <edwardr@senado.leg.br>
2021-02-08 18:21:23 -0300

    Fix #3339

--- sapl/comissoes/models.py
+++ sapl/comissoes/models.py
@@ -345,7 +345,7 @@ class DocumentoAcessorio(models.Model):
     class Meta:
         verbose_name = _('Documento Acessório')
         verbose_name_plural = _('Documentos Acessórios')
-        ordering = ('data', '-data_ultima_atualizacao')
+        ordering = ('data', 'id')
 
     def __str__(self):
         return _('%(nome)s por %(autor)s') % {
