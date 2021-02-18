32b7aae41bd13e6b02cfc3754e9ba2427d62b4c0
eribeiro <edwardr@senado.leg.br>
2021-02-18 14:42:03 -0300

    Conserta bug introduzido por issue PR #3233

--- sapl/materia/forms.py
+++ sapl/materia/forms.py
@@ -475,7 +475,7 @@ class TramitacaoForm(ModelForm):
         unidade_tramitacao_destino = [('', '---------')] + [(ut.pk, ut)
                                                             for ut in ust if ut.comissao and ut.comissao.ativa]
         unidade_tramitacao_destino.extend(
-            [(ut.pk, ut) for ut in ust if ut.orgao or ut.comissao])
+            [(ut.pk, ut) for ut in ust if ut.orgao])
         unidade_tramitacao_destino.extend(
             [(ut.pk, ut) for ut in ust if ut.parlamentar])
         self.fields['unidade_tramitacao_destino'].choices = unidade_tramitacao_destino
