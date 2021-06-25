b08f537c400d2b07947a6a14e71aba8e3e24951c
AlGouvea <alvarohsgouvea@gmail.com>
2021-06-25 09:58:28 -0300

    Iniciada a função de teste

--- sapl/api/test_api.py
+++ sapl/api/test_api.py
@@ -4,3 +4,21 @@ import pytest
 from sapl.parlamentares.models import Legislatura, ComposicaoMesa, Parlamentar,\
  SessaoLegislativa, CargoMesa
 
+@pytest.mark.django_db(transaction=False)
+def test_get_mesa_diretora():
+    #criar legislatura, sessao e parlamentares
+    parlamentar = baker.make(Parlamentar, nome_parlamentar='Joseph Joestar', id=8)
+
+    legislatura = baker.make(Legislatura, id=34)
+
+    sessao = baker.make(SessaoLegislativa, legislatura=legislatura)
+
+    cargo = baker.make(CargoMesa, descricao="presidente")
+
+    #passar informações para a composicao_mesa
+    mesa = baker.make(ComposicaoMesa, parlamentar=parlamentar,
+    sessao_legislativa=sessao, cargo=cargo)
+
+    print(mesa.cargo)
+
+    #checagens  
