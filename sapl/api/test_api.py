e1acbff5f69877ea124fc6491e5d506df391d561
AlGouvea <alvarohsgouvea@gmail.com>
2021-06-28 11:23:54 -0300

    Inicio da configuração do arquivo de testes

--- sapl/api/test_api.py
+++ sapl/api/test_api.py
@@ -1,17 +1,19 @@
 from model_bakery import baker
 import pytest
-
+import json
 from sapl.parlamentares.models import Legislatura, ComposicaoMesa, Parlamentar,\
  SessaoLegislativa, CargoMesa
+from django.urls import reverse
+from sapl.api import views
 
 @pytest.mark.django_db(transaction=False)
-def test_get_mesa_diretora():
+def test_get_mesa_diretora(admin_client):
     #criar legislatura, sessao e parlamentares
-    parlamentar = baker.make(Parlamentar, nome_parlamentar='Joseph Joestar', id=8)
+    parlamentar = baker.make(Parlamentar, nome_parlamentar='Joseph Joestar', id=8, fotografia=None)
 
     legislatura = baker.make(Legislatura, id=34)
 
-    sessao = baker.make(SessaoLegislativa, legislatura=legislatura)
+    sessao = baker.make(SessaoLegislativa, legislatura=legislatura, id=44)
 
     cargo = baker.make(CargoMesa, descricao="presidente")
 
@@ -19,6 +21,10 @@ def test_get_mesa_diretora():
     mesa = baker.make(ComposicaoMesa, parlamentar=parlamentar,
     sessao_legislativa=sessao, cargo=cargo)
 
-    print(mesa.cargo)
+    #Verifica se a mesa foi criada
+    mesa_diretora = ComposicaoMesa.objects.get(sessao_legislativa=sessao, parlamentar=parlamentar)
 
-    #checagens  
+    #Testa o POST
+    jresponse = admin_client.post(reverse('sapl.api:get_mesa_diretora'))
+    assert jresponse.status_code == 200
+
