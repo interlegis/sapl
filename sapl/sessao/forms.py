8a07fba4e2febad9e3a3af82ac9e7dbf5c3bffe8
AlGouvea <alvarohsgouvea@gmail.com>
2021-08-24 11:32:43 -0300

    Retirado o hardcode e adicionado ConsideracoesFinais a tabela auxiliar

--- sapl/sessao/forms.py
+++ sapl/sessao/forms.py
@@ -827,7 +827,7 @@ class ResumoOrdenacaoForm(forms.Form):
             Fieldset(_(''),
                      row1, row2, row3, row4, row5,
                      row6, row7, row8, row9, row10,
-                     row11, row12, row13, row14,
+                     row11, row12, row13, row14, row15,
                      form_actions(label='Atualizar'))
         )
 
