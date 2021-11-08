5f46a550662a310f1124ef0ba2ede53d0ac49278
Edward Oliveira <edwardr@senado.gov.br>
2021-11-08 09:52:50 -0300

    Release: 3.1.162-RC14

--- sapl/settings.py
+++ sapl/settings.py
@@ -41,7 +41,7 @@ ALLOWED_HOSTS = ['*']
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/?next='
 
-SAPL_VERSION = '3.1.162-RC13'
+SAPL_VERSION = '3.1.162-RC14'
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
