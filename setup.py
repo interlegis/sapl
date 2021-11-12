5c0e2e0743a3c031b96edcd582304fc067a46f75
Edward Oliveira <edwardr@senado.gov.br>
2021-11-12 13:29:02 -0300

    Release: 3.1.162-RC18

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ install_requires = [
 ]
 setup(
     name='interlegis-sapl',
-    version='3.1.162-RC17',
+    version='3.1.162-RC18',
     packages=find_packages(),
     include_package_data=True,
     license='GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007',
