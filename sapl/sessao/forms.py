fb2de35ec4b182e9f56b157225b4d3da4d0463ab
Gustavo <gustavocota98@gmail.com>
2022-04-26 09:04:25 -0300

    Reduzindo o número de querys ao banco de dados no Fixes #3496

--- sapl/sessao/forms.py
+++ sapl/sessao/forms.py
@@ -605,12 +605,8 @@ class AdicionarVariasMateriasFilterSet(MateriaLegislativaFilterSet):
 class OradorForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        id_sessao = int(self.initial['id_sessao'])
-        sessao = SessaoPlenaria.objects.get(id=id_sessao)
-        legislatura_vigente = sessao.legislatura
         self.fields['parlamentar'].queryset = \
-            Parlamentar.objects.filter(mandato__legislatura=legislatura_vigente,
-                                       ativo=True).order_by('nome_parlamentar')
+            Parlamentar.objects.filter(mandato__legislatura__sessaoplenaria=self.initial['id_sessao'], ativo=True).order_by('nome_parlamentar')
 
     def clean(self):
         super(OradorForm, self).clean()
@@ -647,12 +643,8 @@ class OradorForm(ModelForm):
 class OradorExpedienteForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        id_sessao = int(self.initial['id_sessao'])
-        sessao = SessaoPlenaria.objects.get(id=id_sessao)
-        legislatura_vigente = sessao.legislatura
         self.fields['parlamentar'].queryset = \
-            Parlamentar.objects.filter(mandato__legislatura=legislatura_vigente,
-                                       ativo=True).order_by('nome_parlamentar')
+            Parlamentar.objects.filter(mandato__legislatura__sessaoplenaria=self.initial['id_sessao'], ativo=True).order_by('nome_parlamentar')
 
     def clean(self):
         super(OradorExpedienteForm, self).clean()
@@ -687,12 +679,8 @@ class OradorExpedienteForm(ModelForm):
 class OradorOrdemDiaForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        id_sessao = int(self.initial['id_sessao'])
-        sessao = SessaoPlenaria.objects.get(id=id_sessao)
-        legislatura_vigente = sessao.legislatura
         self.fields['parlamentar'].queryset = \
-            Parlamentar.objects.filter(mandato__legislatura=legislatura_vigente,
-                                       ativo=True).order_by('nome_parlamentar')
+            Parlamentar.objects.filter(mandato__legislatura__sessaoplenaria=self.initial['id_sessao'], ativo=True).order_by('nome_parlamentar')
 
     def clean(self):
         super(OradorOrdemDiaForm, self).clean()
