3b145209151ab8fa006603deee41bfc9dab3526d
Edward <9326037+edwardoliveira@users.noreply.github.com>
2022-05-16 10:08:35 -0300

    Revert "Exibe no painel a condição de Matéria em Discussão, anterior à Votação (#3554)" (#3556)
    This reverts commit f23c6f61203d663a1081d790d2039cbdb7453a9f.
    
    Co-authored-by: Edward <9326037+edwardoliveira@users.noreply.github.com>

--- sapl/painel/views.py
+++ sapl/painel/views.py
@@ -355,7 +355,7 @@ def get_cronometro_status(request, name):
 
 def get_materia_aberta(pk):
     return OrdemDia.objects.filter(
-        sessao_plenaria_id=pk).filter(Q(votacao_aberta=True) | Q(resultado='Matéria em Discussão')).last()
+        sessao_plenaria_id=pk, votacao_aberta=True).last()
 
 
 def get_presentes(pk, response, materia):
@@ -437,7 +437,7 @@ def get_presentes(pk, response, materia):
 
 def get_materia_expediente_aberta(pk):
     return ExpedienteMateria.objects.filter(
-        sessao_plenaria_id=pk).filter(Q(votacao_aberta=True) | Q(resultado='Matéria em Discussão')).last()
+        sessao_plenaria_id=pk, votacao_aberta=True).last()
 
 
 def response_nenhuma_materia(response):
@@ -563,8 +563,7 @@ def get_dados_painel(request, pk):
         'cronometro_ordem': get_cronometro_status(request, 'ordem'),
         'cronometro_consideracoes': get_cronometro_status(request, 'consideracoes'),
         'status_painel': sessao.painel_aberto,
-        'brasao': brasao,
-        'discutir': False
+        'brasao': brasao
     }
 
     ordem_dia = get_materia_aberta(pk)
@@ -573,14 +572,10 @@ def get_dados_painel(request, pk):
     # Caso tenha alguma matéria com votação aberta, ela é mostrada no painel
     # com prioridade para Ordem do Dia.
     if ordem_dia:
-        if ordem_dia.resultado == 'Matéria em Discussão':
-            response.update({'discutir':True})
         return JsonResponse(get_votos(
             get_presentes(pk, response, ordem_dia),
             ordem_dia))
     elif expediente:
-        if expediente.resultado == 'Matéria em Discussão':
-            response.update({'discutir':True})
         return JsonResponse(get_votos(
             get_presentes(pk, response, expediente),
             expediente))
