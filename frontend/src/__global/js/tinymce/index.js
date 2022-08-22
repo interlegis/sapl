4d7c96f41f477c3e8aa902f912a27c4f3cb9613c
LeandroJatai <leandroroberto.br@gmail.com>
2022-08-22 18:14:35 -0300

    Atualiz de versões do front e impactos no código
    Atualiza versões secundárias e de remendos x.Y.Z
    
    Atualiza versões secundárias (minor version x.Y.z)
    e de remendos (patch version x.y.Z)
    em conjunto das libs de menor impacto no frontend.
    
    Syncs webpack-bundle-tracker django-webpack-loader
    
    A primeira delas é do frontend já a segunda é do backend.
    Elas fazem a ponte entre o front e back. A primeira faz um catálogo
    (webpack-stats-json) de todo material compilado para produção pelo webpack.
    Em tempo de desenvolvimento, esses catálogo não possui referências para
    arquivos, mas para urls do servidor de desenvolvimento do frontend.
    
    Já a segunda, lê todo o material produzido pela primeira, disponibiliza
    filters para acesso geral, modular ou diretamente a arquivos
    específicos.
    
    bump vs secundária do vue e vue-template-compiler
    
    Já existe atualização de versão primária do VueJs mas atualizá-la algo
    em ~3.0.0 exige revisão, refatoração e/ou reimplementação de código.
    
    vue-template-compiler anda junto com vue e é usado para possui compilar
    container pre-alimentado no backend como é o caso de:
    https://github.com/interlegis/sapl/blob/3.1.x/sapl/templates/parlamentares/parlamentares_list.html
    
    migra libs para devDependencies
    
    bump version frontend

--- frontend/src/__global/js/tinymce/index.js
+++ frontend/src/__global/js/tinymce/index.js
@@ -1,37 +1,32 @@
-
-import tinymce from 'tinymce/tinymce'
-import './langs/pt_BR.js'
+import tinymce from 'tinymce'
 
 import 'tinymce/themes/silver'
 import 'tinymce/icons/default'
+import 'tinymce/models/dom/index'
+import 'tinymce/skins/ui/oxide/skin.min.css'
 
-import 'tinymce/plugins/table'
-import 'tinymce/plugins/lists'
 import 'tinymce/plugins/code'
-import 'tinymce/plugins/visualblocks'
+import 'tinymce/plugins/advlist'
+import 'tinymce/plugins/link'
+import 'tinymce/plugins/lists'
+import 'tinymce/plugins/table'
 
-import 'tinymce/skins/ui/oxide/skin.css'
+import './langs/pt_BR.js'
+// import contentUiCss from 'tinymce/skins/ui/oxide/content.css'
+// import contentCss from 'tinymce/skins/content/default/content.css'
 
 window.tinymce = tinymce
-
-window.removeTinymce = function () {
-  while (window.tinymce.editors.length > 0) {
-    window.tinymce.remove(window.tinymce.editors[0])
-  }
-}
-
 window.initTextRichEditor = function (elements, readonly = false) {
-  window.removeTinymce()
   const configTinymce = {
     selector: elements === null || elements === undefined ? 'textarea' : elements,
-    forced_root_block: '',
-    min_height: 200,
     language: 'pt_BR',
-    branding: false,
-    content_css: 'default',
-    plugins: ['lists table code visualblocks'],
-    menubar: 'edit view format table tools',
-    toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent'
+    // skin: false,
+    // content_css: false,
+    forced_root_block: 'div',
+    // content_style: contentUiCss.toString() + '\n' + contentCss.toString(),
+    plugins: 'table lists advlist link code',
+    toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link | code',
+    menubar: 'file edit view insert format table'
   }
   if (readonly) {
     configTinymce.readonly = 1
