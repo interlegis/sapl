f045e5ebd556a8a3ebd426b0db23fca1d1264e58
Edward Oliveira <edwardr@senado.gov.br>
2023-07-17 19:29:43 -0700

    Hot-fix: endpoint do prometheus endpoint URL

--- sapl/urls.py
+++ sapl/urls.py
@@ -69,7 +69,7 @@ urlpatterns += [
     path("robots.txt", TemplateView.as_view(
         template_name="robots.txt", content_type="text/plain")),
 
-    path('prometheus/', include('django_prometheus.urls')),
+    path(r'', include('django_prometheus.urls')),
 
 ]
 
