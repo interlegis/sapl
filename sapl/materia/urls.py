0bc4ae56611d067fb894c769adbebaf616947869
Edward Oliveira <edwardr@senado.gov.br>
2025-08-27 15:40:50 -0300

    Fix PDF report crash

--- sapl/materia/urls.py
+++ sapl/materia/urls.py
@@ -142,8 +142,7 @@ urlpatterns_proposicao = [
         name='proposicao-recebida'),
     re_path(r'^proposicao/devolvida/', ProposicaoDevolvida.as_view(),
         name='proposicao-devolvida'),
-    re_path(r'^proposicao/confirmar/P(?P<hash>[0-9A-Fa-f]+)/'
-        '(?P<pk>\d+)', ConfirmarProposicao.as_view(),
+    re_path(r'^proposicao/confirmar/P(?P<hash>[0-9A-Fa-f]+)/(?P<pk>\d+)', ConfirmarProposicao.as_view(),
         name='proposicao-confirmar'),
     path('sistema/proposicao/tipo/',
         include(TipoProposicaoCrud.get_urls())),
