7c8bca3a0b94d80fdcbf8e558c6c6f81ae9e638c
João Moraes <joaohort@senado.leg.br>
2026-06-23 19:34:58 -0300

    feat(sessao): renomeia anexo para 'Votações Nominais' e exibe anexos na busca
    Renomeia o verbose_name do campo upload_anexo de SessaoPlenaria de 'Anexo da
    Sessão' para 'Votações Nominais' (com migração) e passa a exibir os links de
    Pauta da Sessão e Votações Nominais ao lado da Ata na pesquisa pública de
    sessões plenárias.

--- sapl/sessao/models.py
+++ sapl/sessao/models.py
@@ -209,7 +209,7 @@ class SessaoPlenaria(models.Model):
         null=True,
         storage=OverwriteStorage(),
         upload_to=anexo_upload_path,
-        verbose_name=_('Anexo da Sessão'))
+        verbose_name=_('Votações Nominais'))
     iniciada = models.BooleanField(
         null=True,
         blank=True,
