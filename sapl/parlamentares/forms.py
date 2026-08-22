08c3a69dd6806552a712c0e47b1818be52681280
LeandroJatai <leandroroberto.br@gmail.com>
2026-08-22 14:20:48 -0300

    fix: https://github.com/interlegis/sapl/pull/3829#discussion_r3182291993

--- sapl/parlamentares/forms.py
+++ sapl/parlamentares/forms.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 import logging
 
-from crispy_forms.layout import Fieldset, Layout, Field
+from crispy_forms.layout import Fieldset, Layout
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group, User
